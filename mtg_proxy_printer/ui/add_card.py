bbf48b22bbec6d104295ab970a5d145bb9d18303
thomas <>
2020-11-17 14:33:29 +0000

    AddCardWidget: Implemented the Reset button logic.
    FossilOrigin-Name: 0d8aeffd577923d75bb5f82784c5a5f3d538db179556cc0a01a23a2de8984e36

--- mtg_proxy_printer/ui/add_card.py
+++ mtg_proxy_printer/ui/add_card.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QDialogButtonBox, QLineEdit, QSpinBox
 
 
 from mtg_proxy_printer.ui.common import inherits_from_ui_file_with_name
@@ -28,4 +28,26 @@ class AddCardWidget(*inherits_from_ui_file_with_name("add_card_widget")):
     def __init__(self, parent: QWidget = None):
         super(AddCardWidget, self).__init__(parent)
         self.setupUi(self)
+        self.card_name_search: QLineEdit
+        self.set_name_search: QLineEdit
+        self.collectors_number_search: QLineEdit
+        self.copies_input: QSpinBox
+        self.scryfall_url_input: QLineEdit
+        self._connect_reset_button()
+        self.button_box.button(QDialogButtonBox.Ok).clicked.connect(self.on_ok_button_triggered)
+
         logger.info(f"Created {self.__class__.__name__} instance.")
+
+    def _connect_reset_button(self):
+        logger.debug("User reset the add_card_widget form.")
+        self.button_box: QDialogButtonBox
+        reset_button_clicked_signal = self.button_box.button(QDialogButtonBox.Reset).clicked
+        reset_button_clicked_signal.connect(self.card_name_search.clearEditText)
+        reset_button_clicked_signal.connect(self.set_name_search.clearEditText)
+        reset_button_clicked_signal.connect(self.collectors_number_search.clearEditText)
+        reset_button_clicked_signal.connect(lambda: self.copies_input.setValue(1))
+        reset_button_clicked_signal.connect(self.scryfall_url_input.clear)
+
+    def on_ok_button_triggered(self):
+        logger.debug("User clicked OK and adds a new card to the current page.")
+        pass
