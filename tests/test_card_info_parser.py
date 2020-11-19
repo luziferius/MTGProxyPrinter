dba8f1632c03c374f18b3de943a85e24e415be5b
thomas <>
2020-11-19 12:34:59 +0000

    Tests: Added database content verification. Added test for split cards.
    FossilOrigin-Name: 39f343e53956473bfc99770e805d17acc77c8da3488ba12746ddb7833da41ed6

--- tests/test_card_info_parser.py
+++ tests/test_card_info_parser.py
@@ -17,6 +17,8 @@ import json
 import pkg_resources
 import typing
 
+from hamcrest import *
+
 import mtg_proxy_printer.model.carddb
 from mtg_proxy_printer.card_info_importer import populate_database, JSONType
 
@@ -27,9 +29,91 @@ def load_json(name: str) -> typing.Generator[JSONType, None, None]:
     )
 
 
+def _assert_set_contains(
+        model: mtg_proxy_printer.model.carddb.CardDatabase,
+        values: typing.Iterable[typing.Tuple[str, str, str]]):
+    assert_that(
+        model.db.execute('SELECT "set", set_name, set_uri FROM "Set"').fetchall(),
+        contains_inanyorder(
+            *values
+        ),
+        "Set relation contains unexpected data"
+    )
+
+
+def _assert_card_contains(
+        model: mtg_proxy_printer.model.carddb.CardDatabase,
+        values: typing.Iterable[typing.Tuple[str, str, str, str, str, bool]]):
+    assert_that(
+        model.db.execute(
+            'SELECT scryfall_id, oracle_id, "set", collector_number, language, highres_image FROM Card').fetchall(),
+        contains_inanyorder(
+            *values
+        ),
+        "Card relation contains unexpected data"
+    )
+
+
+def _assert_card_faces_contains(
+        model: mtg_proxy_printer.model.carddb.CardDatabase,
+        values: typing.Iterable[typing.Tuple[str, str, str]]):
+    assert_that(
+        model.db.execute("SELECT scryfall_id, card_name, png_image_uri FROM CardFace").fetchall(),
+        contains_inanyorder(
+            *values
+        ),
+        "CardFaces relation contains unexpected data"
+    )
 
 def test_import_double_faced():
+    """
+    Double faced card. Any transform/meld/modal double faced card.
+    """
     model = mtg_proxy_printer.model.carddb.CardDatabase(":memory:")
     data = load_json("double_faced")
     populate_database(model, data)
+    _assert_set_contains(model, [
+        ("xln", "Ixalan", "https://scryfall.com/sets/xln?utm_source=api"),
+    ])
+    _assert_card_contains(model, [
+        ("000847d3-ebde-4580-a00e-61d501e99485", "ea9c459a-6047-43aa-968f-a582be4000e8", "xln", "191", "zhs", False),
+    ])
+    _assert_card_faces_contains(model, [
+        ("000847d3-ebde-4580-a00e-61d501e99485", "伊替莫成长仪式", "https://c1.scryfall.com/file/scryfall-cards/png/front/0/0/000847d3-ebde-4580-a00e-61d501e99485.png?1562549619"),
+        ("000847d3-ebde-4580-a00e-61d501e99485", "烈阳育所伊替莫", "https://c1.scryfall.com/file/scryfall-cards/png/back/0/0/000847d3-ebde-4580-a00e-61d501e99485.png?1562549619"),
+    ])
 
+
+def test_import_split_card():
+    """Has two or more smaller cards on one side."""
+    model = mtg_proxy_printer.model.carddb.CardDatabase(":memory:")
+    # Loads Korean printing of "Cut // Ribbons"
+    data = load_json("split_card")
+    populate_database(model, data)
+    _assert_set_contains(model, [
+        ("ahk", "Amonkhet", "https://scryfall.com/sets/akh?utm_source=api"),
+    ])
+    _assert_card_contains(model, [
+        ("00031562-3818-49f9-b45c-ab28a521284c", "98a5bf1a-1088-4339-9a9b-6ee5e4956cf1", "akh", "223", "ko", False),
+    ])
+    _assert_card_faces_contains(model, [
+        ("00031562-3818-49f9-b45c-ab28a521284c", "절단", "https://c1.scryfall.com/file/scryfall-cards/png/front/0/0/00031562-3818-49f9-b45c-ab28a521284c.png?1540283200"),
+        ("00031562-3818-49f9-b45c-ab28a521284c", "띠", "https://c1.scryfall.com/file/scryfall-cards/png/front/0/0/00031562-3818-49f9-b45c-ab28a521284c.png?1540283200"),
+    ])
+
+
+"""
+Assert-template:
+
+_assert_set_contains(model, [
+    ("", "", ""),
+])
+_assert_card_contains(model, [
+    ("", "", "", "", "", True),
+])
+_assert_card_faces_contains(model, [
+    ("", "", ""),
+    ("", "", ""),
+])
+
+"""
