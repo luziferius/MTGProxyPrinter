60e8fbd57b4602d7fce6216217a5097c04c8d2e8
thomas <>
2020-11-27 15:41:03 +0000

    PageRenderer: Implemented image grid placement in the PageScene. Images are now placed in a grid, based on the margins and spacing options set in the Document instance.
    FossilOrigin-Name: 7fcd85d117c8e9cf5be89ed5c9e49d5f8fad183b2aca612acc052b69dd1a0a08

--- mtg_proxy_printer/ui/page_renderer.py
+++ mtg_proxy_printer/ui/page_renderer.py
@@ -13,10 +13,71 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QGraphicsView
+import random
 
+from PyQt5.QtCore import pyqtSlot, QRectF, QPointF, QSizeF, Qt, QModelIndex
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt5.QtGui import QColor, QPixmap
+import pint
+
+from mtg_proxy_printer.settings import settings
 from mtg_proxy_printer.model.document import Page, Document
+
+unit_registry = pint.UnitRegistry()
+DPI: pint.Quantity = 300 / unit_registry.inch
+
+
+class PageScene(QGraphicsScene):
+    IMAGE_WIDTH: pint.Quantity = unit_registry("63 millimeter")
+    IMAGE_HEIGHT: pint.Quantity = unit_registry("88 millimeter")
+
+
+    @pyqtSlot(QModelIndex)
+    def draw_card(self, index: QModelIndex):
+        print(f"draw_card({index.row(), index.column()=})")
+        position = self._compute_position_for_image(index)
+        image: QPixmap = index.sibling(index.row(), 4).data(Qt.DisplayRole)
+        pixmap = self.addPixmap(image)
+        pixmap.setPos(position)
+        pixmap.setScale()
+
+    @pyqtSlot()
+    def redraw(self):
+        self.clear()
+        page: Page = self.parent().page
+        self.addRect(self.sceneRect(), QColor(200, 200, 10), QColor(150, 130, 120))
+        for index in (page.createIndex(row, 0) for row in range(page.rowCount())):
+            self.draw_card(index)
+
+    def _compute_position_for_image(self, index: QModelIndex):
+        document = self.get_document()
+        cards_per_row = self._compute_cards_per_row()
+        column = index.row() % cards_per_row
+        row = index.row() // cards_per_row
+        spacing_vertical = document.image_spacing_vertical * unit_registry.millimeter
+        spacing_horizontal = document.image_spacing_horizontal * unit_registry.millimeter
+
+        x_pos = document.margin_left * unit_registry.millimeter + column * (PageScene.IMAGE_WIDTH + spacing_vertical)
+        y_pos = document.margin_top * unit_registry.millimeter + row * (PageScene.IMAGE_HEIGHT + spacing_horizontal)
+        return QPointF(
+            (x_pos * DPI).to_reduced_units().to_tuple()[0],
+            (y_pos * DPI).to_reduced_units().to_tuple()[0],
+        )
+
+    def _compute_cards_per_row(self) -> int:
+        document = self.get_document()
+        total_width: pint.Quantity = document.page_width * unit_registry.millimeter
+        margins: pint.Quantity = (document.margin_left + document.margin_right) * unit_registry.millimeter
+        spacing: pint.Quantity = document.image_spacing_vertical * unit_registry.millimeter
+
+        total_width -= margins + PageScene.IMAGE_WIDTH
+        cards = total_width/(PageScene.IMAGE_WIDTH+spacing)+1
+        return int(cards.to_tuple()[0])
+
+    def get_document(self) -> Document:
+        page: Page = self.parent().page
+        document: Document = page.parent()
+        return document
 
 
 class PageRenderer(QGraphicsView):
@@ -24,14 +85,34 @@ class PageRenderer(QGraphicsView):
     def __init__(self, *args, **kwargs):
         super(PageRenderer, self).__init__(*args, **kwargs)
         self.page = None
+        self.setScene(PageScene(self._get_document_size(), self))
 
-    @pyqtSlot()
+    @pyqtSlot(Page)
     def set_page(self, page: Page):
+        print(f"set_page called: {page=}")
+        if page is None:
+            self.scene().clear()
+        else:
+            if self.page is not None:
+                self.page.dataChanged.disconnect(self.scene().draw_card)
+            self.page = page
+            self.page.dataChanged.connect(self.scene().draw_card)
+            self.scene().redraw()
 
-        self.page = page
-        self.page.dataChanged.connect(self.redraw)
+    def _get_document_size(self) -> QRectF:
+        document_settings = settings["documents"]
+        height: pint.Quantity = document_settings.getint("paper-height-mm") * unit_registry.millimeter
+        width: pint.Quantity = document_settings.getint("paper-width-mm") * unit_registry.millimeter
+        page_size = QRectF(
+            QPointF(0, 0),
+            QSizeF(
+                (DPI*width).to_reduced_units().to_tuple()[0],
+                (DPI*height).to_reduced_units().to_tuple()[0]
+            )
+        )
+        return page_size
 
     @pyqtSlot()
-    def redraw(self):
-        document: Document = self.page.parent()
-        self.resize(document.page_width, document.page_height)
+    def on_resize_event_triggered(self):
+        pass
+        #self.fitInView(self.scene().sceneRect(), Qt.KeepAspectRatio)
