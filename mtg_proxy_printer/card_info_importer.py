1adff1a13056fa0f64562a4f43fc576db9e3f253
thomas <>
2020-12-01 11:19:18 +0000

    CardInfoDownloader: Rewritten card filter to be easily extensible.
    FossilOrigin-Name: 5af7821630dfcd1f000afb929a22a3be653f83d8d3a4acddea6d0367c18fe7c9

--- mtg_proxy_printer/card_info_importer.py
+++ mtg_proxy_printer/card_info_importer.py
@@ -145,19 +145,13 @@ class CardInfoDownloader(QObject):
         """
         Takes an iterable returned by card_info_importer.read_json_card_data() and populates the database with card data.
         """
-        download_cards_with_content_warning = mtg_proxy_printer.settings.settings["downloads"].getboolean(
-            "download-cards-depicting-racism")
         for index, card in enumerate(card_data, start=1):
             if card["object"] != "card":
                 logger.warning(f"Non-card found in card data during import: {card}")
                 continue
-            try:
-                content_warning = card["content_warning"]
-            except KeyError:
-                content_warning = False
-            if content_warning and not download_cards_with_content_warning:
+            if _should_skip_card(card):
                 logger.debug(
-                    f"Skipping card '{card['name']}' with {content_warning=}, because downloading these is disabled.")
+                    f"Skipping card '{card['name']}', because it matches a download filter.")
                 continue
             set_info = _get_set_info(card)
             card_info = _get_card_info(card)
@@ -180,6 +174,15 @@ class CardInfoDownloader(QObject):
         # Populate the sqlite stat tables to give the query optimizer data to work with.
         # This greatly improves query speed.
         model.db.execute("ANALYZE")
+
+
+def _should_skip_card(card: JSONType) -> bool:
+    """Determine, if the given card should be included based on the application settings"""
+    ds = mtg_proxy_printer.settings.settings["downloads"]
+    return any((
+        # Racism filter
+        card.get("content_warning", False) and not ds.getboolean("download-cards-depicting-racism"),
+    ))
 
 
 def _get_set_info(card: JSONType) -> typing.Tuple[str, str, str]:
