97345801f11c808b9d5d908af4e1d2f0ba0cabed
thomas <>
2020-12-02 19:04:16 +0000

    Fixed test cases that test if a filter was successfully applied.
    FossilOrigin-Name: 5a3213ea21be8ebe331bffd8f06a719fed0c3bfa1d1f857ad598127802ad2286

--- tests/test_card_info_parser.py
+++ tests/test_card_info_parser.py
@@ -74,25 +74,29 @@ def _assert_card_faces_contains(
         "CardFaces relation contains unexpected data"
     )
 
+def _assert_relation_is_empty(model: mtg_proxy_printer.model.carddb.CardDatabase, name: str, columns: str):
+    assert_that(
+        model.db.execute(f"SELECT {columns} FROM {name}").fetchall(),
+        is_(empty()), f"{name} contains unexpected data"
+    )
 
-def _assert_model_is_empty(model):
+
+def _assert_model_is_empty(model: mtg_proxy_printer.model.carddb.CardDatabase):
     """
     Checks, if the model is empty. This is used by tests that check if cards are properly skipped based on
     download settings.
     """
+
     assert_that(
         model.db.execute('SELECT "set", set_name, set_uri FROM "Set"').fetchall(),
         is_(empty()), f"Set relation contains unexpected data"
     )
-    assert_that(
-        model.db.execute(
-            'SELECT scryfall_id, oracle_id, "set", collector_number, language, highres_image FROM Card').fetchall(),
-        is_(empty()), "Card relation contains unexpected data"
-    )
-    assert_that(
-        model.db.execute("SELECT scryfall_id, card_name, png_image_uri FROM CardFace").fetchall(),
-        is_(empty()), "CardFaces relation contains unexpected data"
-    )
+    _assert_relation_is_empty(model, "PrintLanguage", 'language_id, "language"')
+    _assert_relation_is_empty(model, "Card", "card_id, oracle_id")
+    _assert_relation_is_empty(model, "FaceName", "face_name_id, card_name, language_id")
+    _assert_relation_is_empty(model, "CardFace", "card_face_id, card_id, set_id, face_name_id, collector_number, scryfall_id, png_image_uri")
+    _assert_relation_is_empty(model, '"Set"', 'set_id, "set", set_name, set_uri')
+    _assert_relation_is_empty(model, "AllPrintings", 'card_name, "set", "language", collector_number, scryfall_id, png_image_uri')
 
 
 def populate_database(model, data):
