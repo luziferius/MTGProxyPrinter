92eb3e516a9b6e3c48698782ed4331a23051aaa9
thomas <>
2020-12-06 11:29:58 +0000

    setup.py: Only run setup() if __name__ == "__main__". This allows importing setup.py as a library module.
    FossilOrigin-Name: fe2f9fb573f9b0dd5e5e37ce0dc61b8d9fc4e6a3f776400c0acea96f3ab7dc0c

--- setup.py
+++ setup.py
@@ -57,56 +57,57 @@ class BuildWithQtResources(setuptools.command.build_py.build_py):
         return compiled_resources_file_path
 
 
-setup(
-    name=project_name,
-    packages=find_packages(exclude=["tests*", "venv", ".pytest_cache"]),
-    include_package_data=True,  # Database schema is included as package data.
-    entry_points={
-        "gui_scripts": [
-            "mtg-proxy-printer = {main_package}.__main__:main".format(main_package=main_package)
-        ]
-    },
-    version=version,
-    description=description,
-    long_description=long_description,
-    python_requires=">=3.8",
-    author="Thomas Hess",
-    author_email="thomas.hess@udo.edu",
-    url="http://1337net.duckdns.org:8080/mtg-proxy-printer/",
-    cmdclass={'build_py': BuildWithQtResources},
-    license="GPLv3+",
-    install_requires=[
-        "appdirs >= 1.4.3",  # 1.4.3 is the first version Supporting Python >= 3.6
-        "PyQt5",
-        "ijson >= 3.1.0",
-        "pint",
-    ],
-    setup_requires=[
-        'setuptools >= 30.3.0',
-    ],
-    extras_require={
-        "dev": [
-            'pytest-runner',
-            'pytest',
-            'pytest-cov',
-            'pytest-xdist',
-            'PyHamcrest >= 1.8.1',
-            'PyQt5-stubs',  # Install the stubs used for type hinting when creating the development environment
-        ]
-    },
-    test_suite="pytest",
-    # list of classifiers: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Environment :: X11 Applications :: Qt',
-        'Environment :: Win32 (MS Windows)',
-        'Intended Audience :: End Users/Desktop',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Games/Entertainment :: Board Games',
-        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        name=project_name,
+        packages=find_packages(exclude=["tests*", "venv", ".pytest_cache"]),
+        include_package_data=True,  # Database schema is included as package data.
+        entry_points={
+            "gui_scripts": [
+                "mtg-proxy-printer = {main_package}.__main__:main".format(main_package=main_package)
+            ]
+        },
+        version=version,
+        description=description,
+        long_description=long_description,
+        python_requires=">=3.8",
+        author="Thomas Hess",
+        author_email="thomas.hess@udo.edu",
+        url="http://1337net.duckdns.org:8080/mtg-proxy-printer/",
+        cmdclass={'build_py': BuildWithQtResources},
+        license="GPLv3+",
+        install_requires=[
+            "appdirs >= 1.4.3",  # 1.4.3 is the first version Supporting Python >= 3.6
+            "PyQt5",
+            "ijson >= 3.1.0",
+            "pint",
+        ],
+        setup_requires=[
+            'setuptools >= 30.3.0',
+        ],
+        extras_require={
+            "dev": [
+                'pytest-runner',
+                'pytest',
+                'pytest-cov',
+                'pytest-xdist',
+                'PyHamcrest >= 1.8.1',
+                'PyQt5-stubs',  # Install the stubs used for type hinting when creating the development environment
+            ]
+        },
+        test_suite="pytest",
+        # list of classifiers: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Environment :: X11 Applications :: Qt',
+            'Environment :: Win32 (MS Windows)',
+            'Intended Audience :: End Users/Desktop',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3 :: Only',
+            'Topic :: Games/Entertainment :: Board Games',
+            'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        ],
+    )
