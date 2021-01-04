cca089dadf558481fa31a816cd91de160cbf8bab
thomas <>
2021-01-04 12:02:26 +0000

    Cleanup the PyInstaller bundling: Removed deleting the compiled resources from the Linux shell script. Also allow passing arbitrary arguments to the pyinstaller call. And remove the executable bit from all shared objects collected in the dist directory. PyInstaller marks all libraries as executable, which is undesirable. So use find to mark them as regular, non-executable files.
    FossilOrigin-Name: 0b7a64cd87565f488f14f2570e24e572d8746376ee53d6d0eadc7a501d93328d

--- create_application_bundle_using_PyInstaller.sh
+++ create_application_bundle_using_PyInstaller.sh
@@ -2,9 +2,11 @@
 
 # Generate an application bundle using PyInstaller.
 
-PARAMETERS="--name MTGProxyPrinter --additional-hooks-dir pyinstaller_hooks mtg-proxy-printer-runner.py"
-# Allow passing in additional parameters, such as --onefile
-pyinstaller "$1" $PARAMETERS
+source venv/bin/activate
 
-# Cleanup: Delete the automatically generated, compiled resources from the source tree after pyinstaller finished.
-rm "mtg_proxy_printer/ui/compiled_resources.py"
+# Allow passing in additional parameters, such as --onefile
+pyinstaller "$@" --name MTGProxyPrinter --additional-hooks-dir pyinstaller_hooks mtg-proxy-printer-runner.py
+
+# pyinstaller sets the executable bit for all bundled libraries. So remove it, both for security (shared objects should
+#not be executable) and for ease of use, as the shell won’t auto-complete non-executables when using ./<tab>
+find . -type f \( -name "*.so" -or -name "*.so.*" \) -print0 | xargs -0 chmod a-x
