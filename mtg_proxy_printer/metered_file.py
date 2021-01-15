2448456dca5aea021ece527656bc3dd465a48cc9
thomas <>
2021-01-15 11:53:51 +0000

    MeteredFile: Properly implement __enter__() and __exit__(). Also do absolute progress reports in the total_bytes_processed signal, instead of reporting relative progress by reporting the size of the last processed chunk. This is more inline with how QProgressBar expects it’s input data and avoids translation steps later in the using code. Also send Begin/End signals when I/O starts and ends. The new progress metering does not distinguish between reading and writing.
    FossilOrigin-Name: 8cefc6f371be7dd3ae590cc2da8b6be11c4110f2451c2550718cd181159d244f

--- mtg_proxy_printer/metered_file.py
+++ mtg_proxy_printer/metered_file.py
@@ -14,15 +14,13 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-from typing import Iterable, List, Optional, TYPE_CHECKING
+from typing import Iterable, List, Optional
 
 from io import BufferedIOBase
 from PyQt5.QtCore import QObject, pyqtSignal
 
 from delegateto import delegate
 
-if TYPE_CHECKING:
-    from _typeshed import WriteableBuffer, ReadableBuffer
 
 __all__ = [
     "MeteredFile",
@@ -32,60 +30,80 @@ __all__ = [
 @delegate(
     "file",
     # IOBase and BufferedIOBase methods
-    "seekable", "seek", "readable", "writable", "close", "fileno", "flush", "isatty", "tell", "truncate", "detach",
-    # Context manager
-    "__enter__", "__exit__",
+    "seekable", "readable", "writable", "close", "fileno", "flush", "isatty", "tell", "truncate", "detach",
 )
 class MeteredFile(QObject):
     """Takes a file-like object and monitors read and write progress."""
-    bytes_read = pyqtSignal(int)
-    bytes_written = pyqtSignal(int)
 
-    def __init__(self, file: BufferedIOBase, expected_size_bytes: int = None, parent: QObject = None):
+    io_begin = pyqtSignal(int)
+    total_bytes_processed = pyqtSignal(int)
+    io_end = pyqtSignal()
+
+    def __init__(self, file: BufferedIOBase, expected_size_bytes: int = 0, parent: QObject = None):
         super(MeteredFile, self).__init__(parent)
         self.file = file
+        self._total_bytes_processed = 0
         self.expected_size_bytes = expected_size_bytes
+
+    def __enter__(self):
+        self.io_begin.emit(self.expected_size_bytes)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> Optional[bool]:
+        try:
+            result = self.file.__exit__(exc_type, exc_val, exc_tb)
+        finally:
+            self.io_end.emit()
+        return result
+
+    def _processed(self, byte_count: int):
+        self._total_bytes_processed += byte_count
+        self.total_bytes_processed.emit(self._total_bytes_processed)
+
+    def seek(self, __offset: int, __whence: int = None):
+        self.file.seek(__offset, __whence)
+        self._total_bytes_processed = __offset
+        self.total_bytes_processed.emit(self._total_bytes_processed)
 
     def read(self, __size: Optional[int] = None) -> bytes:
         buffer = self.file.read(__size)
-        self.bytes_read.emit(len(buffer))
+        self._processed(len(buffer))
         return buffer
 
     def read1(self, __size: int = None) -> bytes:
         buffer = self.file.read1(__size)
-        self.bytes_read.emit(len(buffer))
+        self._processed(len(buffer))
         return buffer
 
     def readinto(self, __buffer) -> int:
         bytes_read = self.file.readinto(__buffer)
-        self.bytes_read.emit(bytes_read)
+        self._processed(bytes_read)
         return bytes_read
 
     def readinto1(self, __buffer) -> int:
         bytes_read = self.file.readinto1(__buffer)
-        self.bytes_read.emit(bytes_read)
+        self._processed(bytes_read)
         return bytes_read
 
     def readline(self, __size: Optional[int] = ...) -> bytes:
         line = self.file.readline(__size)
-        self.bytes_read.emit((len(line)))
+        self._processed(len(line))
         return line
 
     def readlines(self, __hint: int = None) -> List[bytes]:
         lines = self.file.readlines(__hint)
-        self.bytes_read.emit(sum(map(len, lines)))
+        total_bytes = sum(map(len, lines))
+        self._processed(total_bytes)
         return lines
 
     def write(self, __buffer) -> int:
         bytes_written = self.file.write(__buffer)
-        self.bytes_written.emit(bytes_written)
+        self._processed(bytes_written)
         return bytes_written
 
     def writelines(self, __lines: Iterable[bytes]) -> None:
-        def _monitor(__lines: Iterable[ReadableBuffer]):
+        def _monitor(__lines: Iterable[bytes]):
             for line in __lines:
                 yield line
-                self.bytes_written.emit(len(line))
+                self._processed(len(line))
         self.file.writelines(_monitor(__lines))
-
-
