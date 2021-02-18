0f7a97f1afc379fad71c94d916dc15535b357e74
thomas <>
2021-02-18 12:57:57 +0000

    DeckImportWizard: Moved the ChooseListFormatPage to the front and implemented the logic for LoadListPage. On the LoadListPage, the Browse button now allows loading a file from disk into the text editor. The Wizard’s Next button is only active, if the deck list is non-empty.
    FossilOrigin-Name: 05a1e2a0a7f600ee390f64bb9affdbff1db96f96ab7620a52125b721698c14b5

--- mtg_proxy_printer/ui/deck_import_wizard.py
+++ mtg_proxy_printer/ui/deck_import_wizard.py
@@ -13,28 +13,54 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt5.QtWidgets import QWizard, QWizardPage
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QWizard, QWizardPage, QFileDialog, QPlainTextEdit, QMessageBox
 
 from mtg_proxy_printer.model.carddb import CardDatabase
+from mtg_proxy_printer.ui.common import inherits_from_ui_file_with_name
 
 
-class LoadListPage(QWizardPage):
-    pass
+class LoadListPage(*inherits_from_ui_file_with_name("load_list_page")):
+    def __init__(self, *args, **kwargs):
+        super(LoadListPage, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.registerField("deck_list*", self.deck_list, "plainText", self.deck_list.textChanged)
+
+    @pyqtSlot()
+    def on_deck_list_browse_button_clicked(self):
+        self.deck_list: QPlainTextEdit
+        if not self.deck_list.toPlainText() \
+                or QMessageBox.question(
+                        self, "Overwrite existing deck list?",
+                        "Selecting a file will overwrite the existing deck list. Continue?",
+                        QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
+            # Ignore the used file type filter (second return value)
+            selected_file, _ = QFileDialog.getOpenFileName(self, "Select deck file")
+            if selected_file:
+                print(selected_file)
+                self.deck_list.clear()
+                with open(selected_file, "rt") as opened_file:
+                    self.deck_list.setPlainText(opened_file.read())
 
 
 class ChooseListFormatPage(QWizardPage):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(ChooseListFormatPage, self).__init__(*args, **kwargs)
+        # self.setupUi(self)
 
 
 class SummaryPage(QWizardPage):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(SummaryPage, self).__init__(*args, **kwargs)
+        # self.setupUi(self)
 
 
 class DeckImportWizard(QWizard):
     def __init__(self, card_db: CardDatabase, *args, **kwargs):
         super(DeckImportWizard, self).__init__(*args, **kwargs)
         self.card_db = card_db
-        self.addPage(LoadListPage())
         self.addPage(ChooseListFormatPage())
+        self.addPage(LoadListPage())
         self.addPage(SummaryPage())
         self.setWindowTitle("Import a deck list")
