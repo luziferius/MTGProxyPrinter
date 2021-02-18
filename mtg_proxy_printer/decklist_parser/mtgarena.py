a3b10ab9c800bafcde6d7b76a1c726d43c9c88bc
thomas <>
2021-02-18 10:39:36 +0000

    Use class-based design for regular-expression based deck list parsers.
    FossilOrigin-Name: 0f980978dc491b5c248a205457491b6b62ee6b1eb70d78b437ca055ae18b0079

--- mtg_proxy_printer/decklist_parser/mtgarena.py
+++ mtg_proxy_printer/decklist_parser/mtgarena.py
@@ -21,28 +21,59 @@ import typing
 from mtg_proxy_printer.model.carddb import Card, CardDatabase
 import mtg_proxy_printer.settings
 
-line_parser = re.compile(r"(?P<copies>\d+) (?P<name>.+) \((?P<set_abbr>\w+)\) (?P<collector_number>\d+)")
-
 ParsedDeck = typing.Tuple[typing.Counter[Card], typing.List[str]]
 
 
-def parse_deck(card_db: CardDatabase, deck: typing.Union[pathlib.Path, str]) -> ParsedDeck:
-    """Parse an MTG Arena deck list. Compatible with Moxfield exports."""
-    deck_list = deck.read_text() if isinstance(deck, pathlib.Path) else deck
-    add_opposing_faces = mtg_proxy_printer.settings.settings["images"].getboolean("automatically-add-opposing-faces")
-    cards = Counter()
-    unmatched_lines = []
-    for line in deck_list.splitlines():
-        if match := line_parser.match(line):
-            copies = int(match["copies"])
-            # Doesn’t include language information, all cards are implicitly English printings
-            matched_card = Card(match["name"], match["set_abbr"].lower(), match["collector_number"], "en")
-            if card_db.is_valid_and_unique_card(matched_card):
-                card_db.add_missing_information(matched_card)
-                cards[matched_card] += copies
-                if add_opposing_faces and (opposing_face := card_db.get_opposing_face(matched_card)) is not None:
-                    cards[opposing_face] += copies
-        elif line:
-            # Non-empty, non-matching lines
-            unmatched_lines.append(line)
-    return cards, unmatched_lines
+class GenericRegularExpressionDeckParser:
+    """
+    A generic regular expression based parser for deck lists. Takes a regular expression as a Python string and
+    uses that to parse each input line.
+    """
+
+    def __init__(self, card_db: CardDatabase, regular_expression: str):
+        self.card_db = card_db
+        self.parser = re.compile(regular_expression)
+        self.add_opposing_face = mtg_proxy_printer.settings.settings["images"].getboolean(
+            "automatically-add-opposing-faces"
+        )
+
+    def parse_deck(self, deck: typing.Union[pathlib.Path, str]) -> ParsedDeck:
+        """Parse an MTG Arena deck list. Compatible with Moxfield exports."""
+        deck_list = deck.read_text() if isinstance(deck, pathlib.Path) else deck
+        cards = Counter()
+        unmatched_lines = []
+        for line in deck_list.splitlines():
+            # Convert the Match instance to a dict, in order to have get() with a default. The default is used,
+            # if the user-supplied RE doesn’t contain named groups for some of the attributes.
+            if match := self.parser.match(line):
+                match_dict = match.groupdict()
+                copies = int(match_dict.get("copies", 1))
+                # If the matcher doesn’t include language information, all cards are implicitly English printings
+                matched_card = Card(
+                    match_dict.get("name"), match_dict.get("set_code"),
+                    match_dict.get("collector_number"), match_dict.get("language", "en"),
+                    scryfall_id=match_dict.get("scryfall_id"),
+                )
+                if matched_card.set_abbr is not None:
+                    matched_card.set_abbr = matched_card.set_abbr.lower()
+                if self.card_db.is_valid_and_unique_card(matched_card):
+                    self.card_db.add_missing_information(matched_card)
+                    cards[matched_card] += copies
+                    if self.add_opposing_face and (
+                            opposing_face := self.card_db.get_opposing_face(matched_card)) is not None:
+                        cards[opposing_face] += copies
+            elif line:
+                # Non-empty, non-matching lines
+                unmatched_lines.append(line)
+        return cards, unmatched_lines
+
+
+class MTGArenaParser(GenericRegularExpressionDeckParser):
+    """
+    A parser for MTG Arena deck lists. moxfield.com uses this format to export deck lists.
+    """
+    def __init__(self, card_db: CardDatabase):
+        super(MTGArenaParser, self).__init__(
+            card_db,
+            r"(?P<copies>\d+) (?P<name>.+) \((?P<set_code>\w+)\) (?P<collector_number>\d+)"
+        )
