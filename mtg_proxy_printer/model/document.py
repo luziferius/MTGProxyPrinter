1c2d53e4c62f37699512be2a68ba7fac82a97ec1
thomas <>
2021-03-01 13:06:17 +0000

    DocumentLoader: Defend against loading invalid files and SQLite databases that are not MTGProxyPrinter save files. If such a file is loaded, the application is no longer indefinitely trapped in the "loading document" state. Instead, a WARNING is logged, but the document instance remains untouched.
    FossilOrigin-Name: f581cc247f823d7d6208cf4fca21ba03cb0beb027a3cd8ebdf24a132bbdbe9ec

--- mtg_proxy_printer/model/document.py
+++ mtg_proxy_printer/model/document.py
@@ -16,6 +16,7 @@
 import collections
 import itertools
 import pathlib
+import sqlite3
 import typing
 
 import delegateto
@@ -540,15 +541,25 @@ class DocumentLoader(QObject):
     def load_document(self, save_file_path: pathlib.Path):
         logger.info(f"Loading document from {save_file_path}")
         self.loading_state_changed.emit(True)
-        self.document.clear()
-        self.worker.data = self._read_data_from_save_path(save_file_path)
-        self.worker_thread.start()
+        try:
+            self.worker.data = self._read_data_from_save_path(save_file_path)
+        except sqlite3.DatabaseError as e:
+            logger.warning(f"Selected file is not an MTGProxyPrinter document. Not loading it.")
+            # Do not trap the user in an endless loading state, if the selected file is not actually a save file
+            self.loading_state_changed.emit(False)
+            return
+        else:
+            self.document.clear()
+            self.worker_thread.start()
 
     @staticmethod
     def _read_data_from_save_path(save_file_path: pathlib.Path):
         logger.info("Reading data from save file")
         with mtg_proxy_printer.sqlite_helpers.open_database(
                 save_file_path, "document", Document.MIN_SUPPORTED_SQLITE_VERSION) as db:
+            if db.execute("PRAGMA application_id").fetchone()[0] != 41325044:
+                raise sqlite3.DatabaseError("Not an MTGProxyPrinter save file!")
+
             if (schema_version := db.execute("PRAGMA user_version").fetchone()[0]) == 2:
                 query = r"""SELECT page, slot, scryfall_id, 1 AS is_front
                 FROM Card
