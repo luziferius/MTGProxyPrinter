80f498d257e29d43ce2af4c5d0121611f65f1d83
thomas <>
2021-03-26 13:19:41 +0000

    Added test cases for CardDatabase.get_all_languages() and get_card_names().
    FossilOrigin-Name: 5ba9ad5c74fd6df82022cf35d9316d8d08fd261300997929fbcc2d0462335d6d

--- tests/test_carddb.py
+++ tests/test_carddb.py
@@ -13,12 +13,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import typing
+
 from hamcrest import *
+import pytest
 
 from mtg_proxy_printer.model.carddb import CardDatabase
 
-from .helpers import assert_model_is_empty, create_new_card_database_with_json_card
+from .helpers import assert_model_is_empty, create_new_card_database_with_json_card, create_new_card_database_with_multiple_cards
 
+StringList = typing.List[str]
+OptString = typing.Optional[str]
 
 def test_has_data_on_empty_database_returns_false():
     model = CardDatabase(":memory:")
@@ -29,3 +34,37 @@ def test_has_data_on_empty_database_returns_false():
 def test_has_data_on_filled_database_returns_true():
     model = create_new_card_database_with_json_card("regular_english_card")
     assert_that(model.has_data(), is_(True))
+
+
+def test_get_all_languages_without_data():
+    model = CardDatabase(":memory:")
+    assert_that(
+        model.get_all_languages(),
+        is_(empty())
+    )
+
+
+def test_get_all_languages_with_data():
+    model = create_new_card_database_with_multiple_cards("multiple_cards_for_test_card_db")
+    assert_that(
+        model.get_all_languages(),
+        contains_exactly("de", "en", "es")
+    )
+
+
+@pytest.mark.parametrize("language, prefix, expected_names", [
+    ("en", None, ["Forest", "Future Sight"]),
+    ("en", "Fu", ["Future Sight"]),
+    ("en", "%or", ["Forest"]),
+    ("en", "AAAAAAAA", []),
+    ("en", "F%t", ["Forest", "Future Sight"]),
+    ("de", None, ["Wald"]),  # noqa  # A German Forest
+    ("es", None, ["Bosque"]),  # noqa  # A Spanish Forest
+    ("Nonexisting language", None, []),
+])
+def test_get_card_names(language: str, prefix: OptString, expected_names: StringList):
+    model = create_new_card_database_with_multiple_cards("multiple_cards_for_test_card_db")
+    assert_that(
+        model.get_card_names(language, prefix),
+        contains_exactly(*expected_names)
+    )
