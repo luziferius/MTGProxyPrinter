d13899d0f4323073cd3e8c4dfc6094230512171e
thomas <>
2021-05-12 11:42:41 +0000

    Use HTTP instead of HTTPS when checking the chiselapp.com mirror for updates. This is done, because the Python distribution lacks the required Let’s Encrypt certificate, thus causing the update check to  fail.
    FossilOrigin-Name: 2dd740776845e3b485eec6008e49230be7df262aee4296008d0cd26188100747

--- mtg_proxy_printer/update_checker.py
+++ mtg_proxy_printer/update_checker.py
@@ -39,7 +39,7 @@ StringList = typing.List[str]
 OptStr = typing.Optional[str]
 VERSION_TAG_MATCHER = re.compile(r"v(?P<version>\d+\.\d+\.\d+)")
 KNOWN_APPLICATION_MIRRORS: StringList = [
-    "https://chiselapp.com/user/luziferius/repository/MTGProxyPrinter",
+    "http://chiselapp.com/user/luziferius/repository/MTGProxyPrinter",
     "http://1337net.duckdns.org:8080/MTGProxyPrinter",
 ]
 
