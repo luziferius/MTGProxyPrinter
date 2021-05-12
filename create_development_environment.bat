ff6ee9b17090fb88241b800414f60c8445eec2ea
thomas <>
2021-05-12 11:31:38 +0000

    Fix bugs in create_development_environment.bat for Windows. Also use pushd/popd instead of cd to change directories.
    FossilOrigin-Name: 92c42afba7423e4ff797b74db8035cb53d434df4d4b09f5470e3a9e41692169a

--- create_development_environment.bat
+++ create_development_environment.bat
@@ -8,9 +8,9 @@ pip uninstall -y MTGProxyPrinter
 
 :: Cleanup various items that bloat the application bundle. Mostly related to unused PyQt5 and Qt5 components
 
-cd venv\Lib\site-packages
+pushd venv\Lib\site-packages
 
-cd PyQt5
+pushd PyQt5
 
 :: Unused Qsci
 rmdir /S /Q Qt
@@ -20,14 +20,14 @@ rmdir /S /Q Qt5\qml
 
 
 ::  Unused DLLs
-cd Qt5\bin
+pushd Qt5\bin
 del d3dcompiler_47.dll libEGL.dll libGLESv2.dll opengl32sw.dll Qt5Bluetooth.dll Qt5DBus.dll Qt5Designer.dll Qt5Help.dll
 del Qt5Location.dll Qt5Multimedia.dll Qt5MultimediaWidgets.dll Qt5Network.dll Qt5Nfc.dll Qt5OpenGL.dll Qt5Positioning.dll
 del Qt5PositioningQuick.dll Qt5Qml.dll Qt5QmlModels.dll Qt5QmlWorkerScript.dll Qt5Quick.dll Qt5Quick3D.dll
 del Qt5Quick3DAssetImport.dll Qt5Quick3DRender.dll Qt5Quick3DRuntimeRender.dll Qt5Quick3DUtils.dll Qt5QuickControls2.dll
 del Qt5QuickParticles.dll Qt5QuickShapes.dll Qt5QuickTemplates2.dll Qt5QuickTest.dll Qt5QuickWidgets.dll Qt5RemoteObjects.dll
 del Qt5Sensors.dll Qt5SerialPort.dll Qt5Sql.dll Qt5Test.dll Qt5WebChannel.dll Qt5WebSockets.dll Qt5WebView.dll
-cd ..\..
+popd
 
 :: Unused extension modules
 del QAxContainer.pyd  QAxContainer.pyi QtBluetooth.pyd QtBluetooth.pyi QtDBus.pyd QtDBus.pyi QtDesigner.pyd QtDesigner.pyi
@@ -39,19 +39,19 @@ del QtSql.pyd QtSql.pyi QtTest.pyd QtTest.pyi QtTextToSpeech.pyd QtTextToSpeech.
 del QtWebSockets.pyd QtWebSockets.pyi _QOpenGLFunctions_2_0.pyd _QOpenGLFunctions_2_1.pyd _QOpenGLFunctions_4_1_Core.pyd
 
 :: Unused bindings
-cd bindings
-FOR %%binding IN (
+pushd bindings
+FOR %%G IN (
   QAxContainer QtBluetooth QtDBus QtDesigner
   QtHelp QtLocation QtMultimedia QtMultimediaWidgets
   QtNetwork QtNfc QtOpenGL QtPositioning
   QtQml QtQuick QtQuick3D QtQuickWidgets
   QtRemoteObjects QtSensors QtSerialPort QtSql
   QtTest QtTextToSpeech QtWebChannel QtWebSockets
-) DO rmdir /S /Q %%binding
-cd ..
+) DO rmdir /S /Q %%G
+popd
 
 :: leave PyQt5
-cd ..
+popd
 
 :: leave site-packages
-cd ..\..\..
+popd
