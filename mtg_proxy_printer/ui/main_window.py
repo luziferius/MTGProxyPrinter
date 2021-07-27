422ff6c4fba0b4cc78c19436ae2e99b56a8541fe
thomas <>
2021-07-27 18:44:28 +0000

    MainWindow: Applied the same workaround to disconnect the document_view while compacting the document is performed, because this may raise the same IndexError, if the user happens to have the last page selected.
    FossilOrigin-Name: 4b367cc34b4850cfcf85dfd8db06e99f3ffad28811f20199cf14ba9551757f3c

--- mtg_proxy_printer/ui/main_window.py
+++ mtg_proxy_printer/ui/main_window.py
@@ -16,7 +16,7 @@
 import pathlib
 import typing
 
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QStringListModel, QItemSelectionModel, QTimer, QModelIndex
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QStringListModel, QItemSelectionModel, QTimer
 from PyQt5.QtGui import QCloseEvent, QResizeEvent, QShowEvent, QKeySequence
 from PyQt5.QtWidgets import QApplication, QMessageBox, QProgressBar, QAction, QWidget, QToolBar
 from PyQt5.QtTest import QAbstractItemModelTester
@@ -28,7 +28,7 @@ import mtg_proxy_printer.model.imagedb
 import mtg_proxy_printer.model.document
 import mtg_proxy_printer.settings
 import mtg_proxy_printer.print
-from mtg_proxy_printer.ui.common import inherits_from_ui_file_with_name, BlockedSignals
+from mtg_proxy_printer.ui.common import inherits_from_ui_file_with_name
 from mtg_proxy_printer.ui.current_page_view import CurrentPageView
 from mtg_proxy_printer.ui.document_view import DocumentView
 from mtg_proxy_printer.ui.add_card import AddCardWidget
@@ -120,7 +120,6 @@ class MainWindow(*inherits_from_ui_file_with_name(f"{layout}_search_layout/main_
         document.loader.network_error_occurred.connect(self.on_network_error_occurred)
         document.loading_state_changed.connect(self._select_first_page)
         self.action_new_document.triggered.connect(document.clear_all_data)
-        self.action_compact_document.triggered.connect(document.compact_pages)
         self.image_db.add_card.connect(document.add_card)
         if args.file is not None:
             if args.file.is_file():
@@ -253,6 +252,18 @@ class MainWindow(*inherits_from_ui_file_with_name(f"{layout}_search_layout/main_
         QApplication.instance().shutdown()
 
     @pyqtSlot()
+    def on_action_compact_document_triggered(self):
+        # TODO: Investigate, why unsetting the model is needed.
+        #  The document_view’s selection model somehow asks for data using invalid
+        #  indices, when the last page is selected and gets deleted. The only way around seems to be to
+        #  completely disconnect the model, remove the row, then set it again.
+        self.document_view.setModel(None)
+        self.document.compact_pages()
+        # Now reset the model (and reconnect the currentChanged signal, which seems to be disconnected implicitly
+        self.document_view.setModel(self.document)
+        self.document_view.selectionModel().currentChanged.connect(self.document.on_ui_selects_new_page)
+
+    @pyqtSlot()
     def on_action_cleanup_local_image_cache_triggered(self):
         logger.info("User wants to clean up the local image cache")
         wizard = CacheCleanupWizard(self.card_database, self.image_db, self)
@@ -307,7 +318,7 @@ class MainWindow(*inherits_from_ui_file_with_name(f"{layout}_search_layout/main_
                 QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel
             )
             if result == QMessageBox.Yes:
-                self.document.compact_pages()
+                self.on_action_compact_document_triggered()
             return result
         return QMessageBox.No  # No pages can be saved, assume "No" for this case
 
