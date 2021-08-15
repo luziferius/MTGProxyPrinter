64149fc75ab1814ddcae57b40abd4413a2f4f0fb
thomas <>
2021-08-15 18:07:46 +0000

    Moved most application meta data from setup.py to setup.cfg. This makes the dependencies discoverable by IDEs, which wasn’t the case previously, due to the definition via a dict instance.
    FossilOrigin-Name: 3824a73f2c0ebb91fff6055c8a1897ef1d90c880d02cddd859f3180027eb9a0c

--- setup.py
+++ setup.py
@@ -3,28 +3,12 @@
 """setup.py: setuptools control."""
 
 from pathlib import Path
-import re
 import subprocess
 
 from setuptools import setup, find_packages
 import setuptools.command.build_py
 
-project_name = "MTGProxyPrinter"
 main_package = "mtg_proxy_printer"
-script_file = "{main_package}/meta_data.py".format(main_package=main_package)
-description = "MTGProxyPrinter allows efficient printing of Magic: The Gathering cards for playtesting purposes."
-
-
-with open(script_file, "r", encoding="utf-8") as opened_script_file:
-    version = re.search(
-        r"""^__version__\s*=\s*"(.*)"\s*""",
-        opened_script_file.read(),
-        re.M
-        ).group(1)
-
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
 
 
 class BuildWithQtResources(setuptools.command.build_py.build_py):
@@ -54,40 +38,12 @@ class BuildWithQtResources(setuptools.command.build_py.build_py):
 
 
 setup_parameters = dict(
-    name=project_name,
-    packages=find_packages(exclude=["tests*", "venv", ".pytest_cache"]),
-    include_package_data=True,  # Database schema is included as package data.
-    entry_points={
-        "gui_scripts": [
-            "mtg-proxy-printer = {main_package}.__main__:main".format(main_package=main_package),
-        ],
-    },
-    version=version,
-    description=description,
-    long_description=long_description,
-    python_requires=">=3.8",
-    author="Thomas Hess",
-    author_email="thomas.hess@udo.edu",
-    url="http://1337net.duckdns.org:8080/MTGProxyPrinter/index",
     project_urls={
         "Bug Tracker": "http://1337net.duckdns.org:8080/MTGProxyPrinter/ticket",
     },
     cmdclass={
         'build_py': BuildWithQtResources,
     },
-    license="GPLv3+",
-    install_requires=[
-        "appdirs >= 1.4.3",  # 1.4.3 is the first version Supporting Python >= 3.6
-        "PyQt5",
-        "ijson >= 3.1.0",
-        "pint",
-        "delegateto >= 1.5",
-    ],
-    setup_requires=[
-        'setuptools >= 30.3.0',
-        "wheel",
-        "PyQt5",  # includes the "pyrcc5" resource compiler used during the build process. See BuildWithQtResources
-    ],
     extras_require={
         "dev": [
             'cx_Freeze >= 6.6',
@@ -104,20 +60,6 @@ setup_parameters = dict(
         ]
     },
     test_suite="pytest",
-    # list of classifiers: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Environment :: X11 Applications :: Qt',
-        'Environment :: Win32 (MS Windows)',
-        'Intended Audience :: End Users/Desktop',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Games/Entertainment :: Board Games',
-        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
-    ],
 )
 
 if __name__ == "__main__":
