9d18d3b0cf395503ec3a3f1057bfb9662aef3b29
thomas <>
2021-09-09 09:40:55 +0000

    setup.py: Removed dependencies for the "dev" extra, as these are already specified in setup.cfg.
    FossilOrigin-Name: a75c3e851feff62e842f4f4fad3ab31d61a3a3d6492ec798384ec57ccfe094c7

--- setup.py
+++ setup.py
@@ -44,21 +44,6 @@ setup_parameters = dict(
     cmdclass={
         'build_py': BuildWithQtResources,
     },
-    extras_require={
-        "dev": [
-            'cx_Freeze >= 6.6',
-            'pytest-runner',
-            'pytest',
-            'pytest-cov',
-            'pytest-xdist',
-            'pytest-timeout',
-            'pytest-qt',
-            'PyHamcrest >= 1.8.1',
-            'PyQt5-stubs',  # Install the stubs used for type hinting when creating the development environment
-            "sip",
-            "build",
-        ]
-    },
     test_suite="pytest",
 )
 
