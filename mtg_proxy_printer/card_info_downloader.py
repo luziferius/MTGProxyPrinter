232d41b6db2fbbca52da249ee6be5c4f583a9c58
thomas <>
2022-01-22 14:14:49 +0000

    Handle parser errors by catching occurring exceptions and notifying the user.
    New changes to the Scryfall API apparently made the oracle_id property of cards optional at the top-level of card objects.
    This change catches any error and reports it to the user, instead of crashing the application.
    
    FossilOrigin-Name: 0520f0c45401447f82cbe17ac9e5072cc75dcf412e70ab3cbb92e58e71f928f7

--- mtg_proxy_printer/card_info_downloader.py
+++ mtg_proxy_printer/card_info_downloader.py
@@ -219,6 +219,9 @@ class CardInfoDownloadWorker(DownloaderBase):
         except sqlite3.Error as e:
             logger.exception(f"Database error occurred: {e}")
             self.other_error_occurred.emit(str(e))
+        except Exception as e:
+            logger.exception(f"Error in parsing step")
+            self.other_error_occurred.emit(f"Failed to parse data from Scryfall. Reported error: {e}")
         finally:
             _clear_lru_caches()
             logger.info(f"Finished import with {card_count} imported cards.")
@@ -240,21 +243,21 @@ class CardInfoDownloadWorker(DownloaderBase):
         index = 0
         face_ids: IntTuples = []
         for index, card in enumerate(card_data, start=1):
+            if not self.should_run:
+                logger.info(f"Aborting card import after {index} cards due to user request.")
+                self.download_finished.emit()
+                return index
             if card["object"] != "card":
                 logger.warning(f"Non-card found in card data during import: {card}")
                 continue
             if _should_skip_card(card, download_enabled, skip_cards_banned_in_formats):
                 skipped_cards += 1
                 continue
-            if not self.should_run:
-                logger.info(f"Aborting card import after {index} cards due to user request.")
-                self.download_finished.emit()
-                return index
-            language_id = _insert_language(self.model, card["lang"])
-            card_id = _insert_card(self.model, card["oracle_id"])
-            set_id = _insert_set(self.model, card)
-            printing_id = insert_printing(self.model, card, card_id, set_id)
-            face_ids += _insert_card_faces(self.model, card, language_id, printing_id)
+            try:
+                face_ids = self._parse_single_printing(card, face_ids)
+            except Exception as e:
+                logger.exception(f"Error while parsing card at position {index}. {card=}")
+                raise RuntimeError(f"Error while parsing card at position {index}: {e}")
             if not index % 10000:
                 logger.debug(f"Imported {index} cards.")
         _clean_unused_data(self.model.db, face_ids)
@@ -271,6 +274,14 @@ class CardInfoDownloadWorker(DownloaderBase):
         self.model.db.execute("ANALYZE\n")
         self.model.commit()
         return index
+
+    def _parse_single_printing(self, card, face_ids):
+        language_id = _insert_language(self.model, card["lang"])
+        card_id = _insert_card(self.model, card["oracle_id"])
+        set_id = _insert_set(self.model, card)
+        printing_id = insert_printing(self.model, card, card_id, set_id)
+        face_ids += _insert_card_faces(self.model, card, language_id, printing_id)
+        return face_ids
 
 
 def _clear_lru_caches():
