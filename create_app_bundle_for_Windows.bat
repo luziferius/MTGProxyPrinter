2f41cb2046d73d5cb24f53d7a06945d03bd3350f
thomas <>
2022-02-05 23:10:38 +0000

    Refactored bundle creation on Windows. Instead of modifying the site-packages, only the final build is cleaned up after it is built.
    FossilOrigin-Name: d6850bf311aaaa3ba199c575b9dace359f854cfccac310a1c985e898d84c15a1

--- create_app_bundle_for_Windows.bat
+++ create_app_bundle_for_Windows.bat
@@ -10,9 +10,5 @@ IF EXIST "venv" (
 :: Create a platform-dependent, portable build in the build directory
 :: and an MSI-based installer in the dist directory
 python setup_cx_freeze.py build_exe
-pushd build\exe*
-del lib\PyQt5\*.dll
-del lib\ijson\backends\python*.dll
-rmdir /S /Q PyQt5.uic.widget-plugins
-popd
-python setup_cx_freeze.py bdist_msi
+call clean_windows_build.bat
+python setup_cx_freeze.py bdist_msi --skip-build
