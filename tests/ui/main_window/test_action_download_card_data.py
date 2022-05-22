4f49b4d892a3d4e12b752591f755b13f2dbbbfcc
thomas <>
2022-05-22 16:11:16 +0000

    Use the stop_thread() function to stop QThreads in the test cleanup code.
    FossilOrigin-Name: 7d7674e325216ef92dd53421d690c2a8dd6244cb6ed5b790711afdda2eeae0d5

--- tests/ui/main_window/test_action_download_card_data.py
+++ tests/ui/main_window/test_action_download_card_data.py
@@ -23,6 +23,7 @@ from PyQt5.QtWidgets import QApplication, QAction
 from pytestqt.qtbot import QtBot
 from hamcrest import *
 
+from mtg_proxy_printer.stop_thread import stop_thread
 from mtg_proxy_printer.card_info_downloader import CardInfoDownloader
 from mtg_proxy_printer.model.carddb import CardDatabase
 from mtg_proxy_printer.model.imagedb import ImageDatabase
@@ -44,15 +45,9 @@ def main_window(qtbot, card_db: CardDatabase, image_db: ImageDatabase, request) 
         main_window = MainWindow(card_db, cid, image_db, document, QStringListModel(["en"]))
         QApplication.instance().shutdown = unittest.mock.MagicMock()
         yield main_window
-        if document.loader.worker_thread.isRunning():
-            document.loader.worker_thread.quit()
-            document.loader.worker_thread.wait(100)
-        if image_db.download_thread.isRunning():
-            image_db.download_thread.quit()
-            image_db.download_thread.wait(100)
-        if cid.worker_thread.isRunning():
-            cid.worker_thread.quit()
-            cid.worker_thread.wait(100)
+        stop_thread(lambda _: None, document.loader.worker_thread)
+        stop_thread(lambda _: None, image_db.download_thread)
+        stop_thread(lambda _: None, cid.worker_thread)
 
 
 @pytest.mark.parametrize("handled_error", [socket.timeout, urllib.error.URLError("Test reason")])
