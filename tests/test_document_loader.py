3af19362bd972353853283a1b70fe1f4b9ac53c2
thomas <>
2022-06-30 09:18:04 +0000

    Tests: Removed the document_with_filled_card_db pytest fixture, because it was essentially the same as the document fixture.
    FossilOrigin-Name: d3291527793250e10dfa026aa4d15525c1f439f9de4b82e845060382d80f35ac

--- tests/test_document_loader.py
+++ tests/test_document_loader.py
@@ -34,23 +34,6 @@ import mtg_proxy_printer.sqlite_helpers
 from tests.helpers import fill_card_database_with_json_card
 
 
-@pytest.fixture()
-def document_with_filled_card_db(qtbot, card_db: CardDatabase) -> mtg_proxy_printer.model.document.Document:
-    fill_card_database_with_json_card(qtbot, card_db, "regular_english_card")
-    assert_that(card_db.is_scryfall_id_known("0000579f-7b35-4ed3-b44c-db2a538066fe", True), is_(True))
-    image_db = ImageDatabase(pathlib.Path("/tmp"))
-    key = ImageKey("0000579f-7b35-4ed3-b44c-db2a538066fe", True, True)
-    image_db.loaded_images[key] = image_db.blank_image
-    image_db.images_on_disk.add(key)
-    document = mtg_proxy_printer.model.document.Document(card_db, image_db)
-    assert_document_is_empty(document)
-    yield document
-    image_db.quit_background_thread()
-    document.loader.worker_thread.quit()
-    document.loader.worker_thread.wait(100)
-    assert_that(image_db.download_thread.isRunning(), is_(False))
-
-
 @pytest.mark.parametrize("version", [-1, 0, 1, 5, 6])
 def test_unknown_save_version_raises_exception(empty_save_database: sqlite3.Connection, version: int):
     empty_save_database.execute(f"PRAGMA user_version = {version};")
@@ -83,7 +66,7 @@ def disabled_check_constraints(db: sqlite3.Connection):
 
 
 def test_valid_data_loads_correctly(
-        qtbot: QtBot, document_with_filled_card_db: mtg_proxy_printer.model.document.Document,
+        qtbot: QtBot, document: mtg_proxy_printer.model.document.Document,
         empty_save_database: sqlite3.Connection):
     empty_save_database.execute(
         'INSERT INTO "Card" (page, slot, is_front, scryfall_id) VALUES (?, ?, ?, ?)',
@@ -103,27 +86,27 @@ def test_valid_data_loads_correctly(
               VALUES (1, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             """),
         dataclasses.astuple(page_layout))
-    loader = document_with_filled_card_db.loader
+    loader = document.loader
     save_path = pathlib.Path("/tmp/invalid.mtgproxies")
     with unittest.mock.patch("mtg_proxy_printer.model.document.mtg_proxy_printer.sqlite_helpers.open_database") as mock:
         mock.return_value = empty_save_database
         with qtbot.waitSignal(loader.loading_state_changed, timeout=1000, raising=True,
                               check_params_cb=lambda value: not value), \
                 qtbot.waitSignal(loader.worker.loading_file_successful, timeout=1000), \
-                qtbot.waitSignal(document_with_filled_card_db.total_cards_per_page_changed, timeout=1000), \
-                qtbot.waitSignal(document_with_filled_card_db.loading_state_changed, timeout=1000,
+                qtbot.waitSignal(document.total_cards_per_page_changed, timeout=1000), \
+                qtbot.waitSignal(document.loading_state_changed, timeout=1000,
                                  check_params_cb=lambda value: not value):
             loader.load_document(save_path)
         mock.assert_called_once()
-    assert_that(document_with_filled_card_db.rowCount(), is_(equal_to(1)))
-    page_index = document_with_filled_card_db.index(0, 0)
+    assert_that(document.rowCount(), is_(equal_to(1)))
+    page_index = document.index(0, 0)
     assert_that(page_index.isValid())
-    assert_that(document_with_filled_card_db.rowCount(page_index), is_(1))
+    assert_that(document.rowCount(page_index), is_(1))
     assert_that(page_index.child(0, mtg_proxy_printer.model.document.PageColumns.CardName).data(), is_("Fury Sliver"))
-    assert_that(document_with_filled_card_db.save_file_path, is_(equal_to(save_path)))
-    assert_that(document_with_filled_card_db.page_layout, is_(equal_to(page_layout)))
+    assert_that(document.save_file_path, is_(equal_to(save_path)))
+    assert_that(document.page_layout, is_(equal_to(page_layout)))
     assert_that(
-        document_with_filled_card_db.total_cards_per_page,
+        document.total_cards_per_page,
         is_(equal_to(page_layout.compute_page_card_capacity()))
     )
 
@@ -135,7 +118,7 @@ def test_valid_data_loads_correctly(
     zip(itertools.repeat(1), itertools.repeat(1), itertools.repeat(1), [-1, 1.3, -1000.2, "", "ABC", b"binary"]),
 ))
 def test_invalid_data_in_card_columns_raises_exception(
-        qtbot: QtBot, document_with_filled_card_db: mtg_proxy_printer.model.document.Document,
+        qtbot: QtBot, document: mtg_proxy_printer.model.document.Document,
         empty_save_database: sqlite3.Connection, data):
     # Replace the Card table with one that has no implicit type casting
     empty_save_database.execute("DROP TABLE Card")
@@ -146,7 +129,7 @@ def test_invalid_data_in_card_columns_raises_exception(
         contains_exactly(equal_to(data)),
         "Setup failed: Data mismatch"
     )
-    loader = document_with_filled_card_db.loader
+    loader = document.loader
     with unittest.mock.patch("mtg_proxy_printer.model.document.mtg_proxy_printer.sqlite_helpers.open_database") as mock:
         mock.return_value = empty_save_database
         with qtbot.waitSignal(loader.loading_file_failed, timeout=1000, raising=True), \
@@ -157,12 +140,12 @@ def test_invalid_data_in_card_columns_raises_exception(
                 qtbot.assertNotEmitted(loader.worker.request_blank_pixmap):
             loader.load_document(pathlib.Path("/tmp/invalid.mtgproxies"))
         mock.assert_called_once()
-    assert_document_is_empty(document_with_filled_card_db)
-    assert_that(document_with_filled_card_db.save_file_path, is_(none()))
+    assert_document_is_empty(document)
+    assert_that(document.save_file_path, is_(none()))
 
 
 def test_protects_against_infinite_save_data(
-        qtbot: QtBot, document_with_filled_card_db: mtg_proxy_printer.model.document.Document,
+        qtbot: QtBot, document: mtg_proxy_printer.model.document.Document,
         empty_save_database: sqlite3.Connection):
     empty_save_database.execute("DROP TABLE Card")
     # LIMIT clause in the definition below is a safety measure.
@@ -177,7 +160,7 @@ def test_protects_against_infinite_save_data(
             )
         SELECT * FROM card_gen
         """))
-    loader = document_with_filled_card_db.loader
+    loader = document.loader
     with unittest.mock.patch("mtg_proxy_printer.model.document.mtg_proxy_printer.sqlite_helpers.open_database") as mock:
         mock.return_value = empty_save_database
         with qtbot.waitSignal(loader.loading_file_failed, timeout=1000, raising=True), \
@@ -188,12 +171,12 @@ def test_protects_against_infinite_save_data(
                 qtbot.assertNotEmitted(loader.worker.request_blank_pixmap):
             loader.load_document(pathlib.Path("/tmp/invalid.mtgproxies"))
         mock.assert_called_once()
-    assert_document_is_empty(document_with_filled_card_db)
-    assert_that(document_with_filled_card_db.save_file_path, is_(none()))
+    assert_document_is_empty(document)
+    assert_that(document.save_file_path, is_(none()))
 
 
 def test_protects_against_infinite_settings_data(
-        qtbot: QtBot, document_with_filled_card_db: mtg_proxy_printer.model.document.Document,
+        qtbot: QtBot, document: mtg_proxy_printer.model.document.Document,
         empty_save_database: sqlite3.Connection):
     empty_save_database.execute("DROP TABLE DocumentSettings")
     # LIMIT clause in the definition below is a safety measure.
@@ -215,7 +198,7 @@ def test_protects_against_infinite_settings_data(
             )
         SELECT * FROM settings_gen
         """))
-    loader = document_with_filled_card_db.loader
+    loader = document.loader
     with unittest.mock.patch("mtg_proxy_printer.model.document.mtg_proxy_printer.sqlite_helpers.open_database") as mock:
         mock.return_value = empty_save_database
         with qtbot.waitSignal(loader.loading_file_failed, timeout=1000, raising=True), \
@@ -226,8 +209,8 @@ def test_protects_against_infinite_settings_data(
                 qtbot.assertNotEmitted(loader.worker.request_blank_pixmap):
             loader.load_document(pathlib.Path("/tmp/invalid.mtgproxies"))
         mock.assert_called_once()
-    assert_document_is_empty(document_with_filled_card_db)
-    assert_that(document_with_filled_card_db.save_file_path, is_(none()))
+    assert_document_is_empty(document)
+    assert_that(document.save_file_path, is_(none()))
 
 
 @pytest.mark.parametrize("page_type, expected", [
@@ -237,17 +220,17 @@ def test_protects_against_infinite_settings_data(
     (PageType.UNDETERMINED, 9),
 ])
 def test_page_layout_compute_page_card_capacity(
-        document_with_filled_card_db: mtg_proxy_printer.model.document.Document, page_type: PageType, expected: int):
+        document: mtg_proxy_printer.model.document.Document, page_type: PageType, expected: int):
     assert_that(
-        document_with_filled_card_db.page_layout.compute_page_card_capacity(page_type),
+        document.page_layout.compute_page_card_capacity(page_type),
         is_(equal_to(expected))
     )
 
 
 def test_page_layout_compute_page_card_capacity_default_value(
-        document_with_filled_card_db: mtg_proxy_printer.model.document.Document):
+        document):
     assert_that(
-        document_with_filled_card_db.page_layout.compute_page_card_capacity(),
+        document.page_layout.compute_page_card_capacity(),
         is_(equal_to(9))
     )
 
@@ -259,17 +242,17 @@ def test_page_layout_compute_page_card_capacity_default_value(
     (PageType.UNDETERMINED, 3),
 ])
 def test_page_layout_ccompute_page_row_count(
-        document_with_filled_card_db: mtg_proxy_printer.model.document.Document, page_type: PageType, expected: int):
+        document: mtg_proxy_printer.model.document.Document, page_type: PageType, expected: int):
     assert_that(
-        document_with_filled_card_db.page_layout.compute_page_row_count(page_type),
+        document.page_layout.compute_page_row_count(page_type),
         is_(equal_to(expected))
     )
 
 
 def test_page_layout_compute_compute_page_row_count_default_value(
-        document_with_filled_card_db: mtg_proxy_printer.model.document.Document):
+        document):
     assert_that(
-        document_with_filled_card_db.page_layout.compute_page_row_count(),
+        document.page_layout.compute_page_row_count(),
         is_(equal_to(3))
     )
 
@@ -281,16 +264,16 @@ def test_page_layout_compute_compute_page_row_count_default_value(
     (PageType.UNDETERMINED, 3),
 ])
 def test_page_layout_compute_page_column_count(
-        document_with_filled_card_db: mtg_proxy_printer.model.document.Document, page_type: PageType, expected: int):
+        document: mtg_proxy_printer.model.document.Document, page_type: PageType, expected: int):
     assert_that(
-        document_with_filled_card_db.page_layout.compute_page_column_count(page_type),
+        document.page_layout.compute_page_column_count(page_type),
         is_(equal_to(expected))
     )
 
 
 def test_page_layout_compute_page_column_count_default_value(
-        document_with_filled_card_db: mtg_proxy_printer.model.document.Document):
+        document):
     assert_that(
-        document_with_filled_card_db.page_layout.compute_page_column_count(),
+        document.page_layout.compute_page_column_count(),
         is_(equal_to(3))
     )
