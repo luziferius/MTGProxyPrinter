91a674101da942e7c82135e0b9a689b46760684a
thomas <>
2022-10-18 12:33:13 +0000

    Removed the UI files from the resources registry. These will no longer be included in the compiled resources blob.
    FossilOrigin-Name: 93f5b38a170d74b7a7112c99d56883cdbcf1b4b328f131a01b96cd988a03b66a

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -61,6 +61,6 @@ def test_all_resource_files_are_listed_in_resources_qrc(resource_path: Path):
     listed_paths = set(Path(node.text) for node in resource_document.findall("qresource/file"))
     existing_files = list_dir(base_dir)
     for file in existing_files:
-        if file.name == "resources.qrc":
+        if file.name == "resources.qrc" or file.suffix.casefold() == ".ui":
             continue
         assert_that(file, is_in(listed_paths), "File not listed in resources.qrc")
