bf7a31ed2829a6f6b23a3db4534af977f4eecfe6
thomas <>
2022-12-25 22:09:10 +0000

    Added partial implementation of new ActionEditDocumentSettings. The action handles changing document settings, including the handling of overflowing pages, if the page capacity decreases. Implemented is apply(), undo() is still TDB. Also requires a few more test cases. Additional TODO is inserting cards at arbitrary indices in ActionMoveCards for a proper re-flow of cards. apply() currently appends cards moved to the end of the next valid page, instead of inserting them at index 0.
    FossilOrigin-Name: cdfb4441a045f362d57c1782c6f97b1dda9b4ccf328f6b1f024bf573959b9080

---
+++ mtg_proxy_printer/document_controller/edit_document_settings.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2022 Thomas Hess <thomas.hess@udo.edu>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import typing
+
+from ._interface import DocumentAction
+from .move_cards import ActionMoveCards
+from .page_actions import ActionNewPage
+from mtg_proxy_printer.logger import get_logger
+
+from mtg_proxy_printer.units_and_sizes import PageType
+from mtg_proxy_printer.model.document_loader import PageLayoutSettings
+
+if typing.TYPE_CHECKING:
+    from mtg_proxy_printer.model.document import Document
+
+logger = get_logger(__name__)
+del get_logger
+__all__ = [
+    "ActionEditDocumentSettings",
+]
+
+
+class ActionEditDocumentSettings(DocumentAction):
+    """Modify the document settings."""
+
+    def __init__(self, new_settings: PageLayoutSettings):
+        super().__init__()
+        if new_settings.compute_page_card_capacity(PageType.OVERSIZED) < 1:
+            raise ValueError("New document settings must allow at least one card per page")
+        self.new_settings = new_settings
+        self.old_settings: typing.Optional[PageLayoutSettings] = None
+        self.reflow_actions: typing.List[DocumentAction] = []
+
+    def apply(self, document: "Document"):
+        self.old_settings = document.page_layout
+        document.page_layout = self.new_settings
+        if self.old_settings != self.new_settings:
+            document.page_layout_changed.emit()
+        old_capacities = self.old_settings.compute_page_card_capacity(PageType.REGULAR), \
+            self.old_settings.compute_page_card_capacity(PageType.OVERSIZED)
+        new_capacities = self.new_settings.compute_page_card_capacity(PageType.REGULAR), \
+            self.new_settings.compute_page_card_capacity(PageType.OVERSIZED)
+        if new_capacities < old_capacities:
+            self._reflow_document(document)
+        return self
+
+    def _reflow_document(self, document: "Document"):
+        self._reflow_pages_of_type(document, PageType.REGULAR)
+        self._reflow_pages_of_type(document, PageType.OVERSIZED)
+
+    def _reflow_pages_of_type(self, document: "Document", page_type: PageType):
+        pages = document.pages
+        layout = document.page_layout
+        page_capacity = layout.compute_page_card_capacity(page_type)
+
+        current_index = -1
+        while current_index < document.rowCount()-1:
+            current_index += 1
+            current_page = pages[current_index]
+            if not current_page.accepts_card(page_type):
+                continue
+            cards_on_page = len(current_page)
+            if cards_on_page > page_capacity:
+                target_index = self._find_next_page_accepting(document, page_type, current_index)
+                if target_index is None:
+                    self.reflow_actions.append(ActionNewPage().apply(document))
+                    target_index = current_index + 1
+
+                action = ActionMoveCards(current_index, range(page_capacity, cards_on_page), target_index)
+                self.reflow_actions.append(action.apply(document))
+
+    @staticmethod
+    def _find_next_page_accepting(document: "Document", page_type: PageType, index: int) -> typing.Optional[int]:
+        index += 1
+        for found_index, page in enumerate(document.pages[index:], start=index):
+            if page.accepts_card(page_type):
+                return found_index
+        return None
+
+    def undo(self, document: "Document"):
+        raise NotImplementedError("undo() not yet implemented")
+        return self
+
+    def __eq__(self, other):
+        return isinstance(other, ActionEditDocumentSettings) \
+            and other.new_settings == self.new_settings \
+            and other.old_settings == self.old_settings
