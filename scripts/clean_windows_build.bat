b595c01dcaa139dbdd597648fdc07d231014bf64
thomas <>
2023-04-17 14:16:03 +0000

    Optimized packaging. The resulting MSI package is slightly below 30 MiB.
    FossilOrigin-Name: 3bda1d68acaeb6f0490e2e116b0f80b9a4a2eee383a00fffbfea5ab02be302ff

--- scripts/clean_windows_build.bat
+++ scripts/clean_windows_build.bat
@@ -35,6 +35,7 @@ del Qt*Test*
 del QtOpenGL.* QtOpenGLWidgets.*
 del Qt*DBus*
 del Qt*JsonRpc*
+del Qt*Sql*
 
 pushd translations
 :: Remove translations for unused/removed components
@@ -44,12 +45,21 @@ popd
 
 pushd plugins
 :: The app uses Python's sqlite3 module, thus Qt's sqldrivers aren't used
-rmdir /S /Q sqldrivers qmltooling designer
+rmdir /S /Q sqldrivers qmltooling designer assetimporters
+pushd imageformats
+:: Unused image format libraries, around 1.3 MiB
+del qwebp.dll qtiff.dll qjpeg.dll
+popd
 ::leave plugins
 popd
 
 :: leave PySide6
 popd
+
+
+del shiboken6\shiboken6*.lib
+
+
 :: leave lib
 popd
 ::leave build directory
