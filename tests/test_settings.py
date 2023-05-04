7d01a5a7869d28ad8f87d92e2d6fc3392fa840bf
thomas <>
2023-05-04 10:13:27 +0000

    Unified tests for application settings, reducing boilerplate setup code.
    FossilOrigin-Name: b57e133c6c3cb733c31c6ee128da2b4625c441396decb8813c186194c3dd3b05

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -13,11 +13,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import typing
 import configparser
 import mtg_proxy_printer.settings
 
 import pytest
 from hamcrest import *
+
+
+class Setting(typing.NamedTuple):
+    key: str
+    value: str
 
 
 @pytest.fixture()
@@ -27,9 +33,19 @@ def default_settings() -> configparser.ConfigParser:
     return settings
 
 
+def _setup_section(settings: configparser.ConfigParser, section: str, setting: Setting) -> configparser.SectionProxy:
+    section_proxy = settings[section]
+    assert_that(section_proxy[setting.key], is_not(setting.value), "Test setup failed")
+    assert_that(
+        mtg_proxy_printer.settings.DEFAULT_SETTINGS["documents"][setting.key],
+        is_not(setting.value), "Test setup failed"
+    )
+    section_proxy[setting.key] = setting.value
+    return section_proxy
+
+
 def test__validate_documents_section_restore_horizontal_paper_dimensions(default_settings):
-    documents_section = default_settings["documents"]
-    documents_section["paper-width-mm"] = "10"
+    documents_section = _setup_section(default_settings, "documents", Setting("paper-width-mm", "10"))
     mtg_proxy_printer.settings.validate_settings(default_settings)
     assert_that(documents_section, has_entries({
         "paper-width-mm": equal_to(mtg_proxy_printer.settings.DEFAULT_SETTINGS["documents"]["paper-width-mm"]),
@@ -39,8 +55,7 @@ def test__validate_documents_section_restore_horizontal_paper_dimensions(default
 
 
 def test__validate_documents_section_restore_vertical_paper_dimensions(default_settings):
-    documents_section = default_settings["documents"]
-    documents_section["paper-height-mm"] = "10"
+    documents_section = _setup_section(default_settings, "documents", Setting("paper-height-mm", "10"))
     mtg_proxy_printer.settings.validate_settings(default_settings)
     assert_that(documents_section, has_entries({
         "paper-height-mm": equal_to(mtg_proxy_printer.settings.DEFAULT_SETTINGS["documents"]["paper-height-mm"]),
@@ -50,9 +65,8 @@ def test__validate_documents_section_restore_vertical_paper_dimensions(default_s
 
 
 def test__validate_document_section_restores_duplex_mode(default_settings):
-    documents_section = default_settings["documents"]
-    documents_section["duplex-mode"] = "invalid"
+    documents_section = _setup_section(default_settings, "documents", setting := Setting("duplex-mode", "invalid"))
     mtg_proxy_printer.settings.validate_settings(default_settings)
     assert_that(documents_section, has_entries({
-        "duplex-mode": equal_to(mtg_proxy_printer.settings.DEFAULT_SETTINGS["documents"]["duplex-mode"]),
+        setting.key: equal_to(mtg_proxy_printer.settings.DEFAULT_SETTINGS["documents"][setting.key]),
     }))
