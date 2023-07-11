e25d544dde340955875ca61317b8284f2b63010d
thomas <>
2023-07-11 14:01:08 +0000

    Fix crash when trying to translate a custom card
    FossilOrigin-Name: efe3aea945901ed20605fc4eac653b954bc0e902ea7aff6abca2449718fe0bda

--- mtg_proxy_printer/ui/item_delegates.py
+++ mtg_proxy_printer/ui/item_delegates.py
@@ -40,9 +40,10 @@ class ComboBoxItemDelegate(QStyledItemDelegate):
     def setEditorData(self, editor: QComboBox, index: QModelIndex) -> None:
         model: typing.Union[Document, QSortFilterProxyModel] = index.model()
         source_model: Document = model
+        column = index.column()
         while hasattr(source_model, "sourceModel"):  # Resolve the source model to gain access to the card database.
             source_model = source_model.sourceModel()
-        if index.column() == PageColumns.Set:
+        if column == PageColumns.Set:
             matching_sets = source_model.card_db.find_sets_matching(
                 index.siblingAtColumn(PageColumns.CardName).data(Qt.EditRole),
                 index.siblingAtColumn(PageColumns.Language).data(Qt.EditRole),
@@ -56,7 +57,7 @@ class ComboBoxItemDelegate(QStyledItemDelegate):
                     current_set_position = position
             editor.setCurrentIndex(current_set_position)
 
-        elif index.column() == PageColumns.CollectorNumber:
+        elif column == PageColumns.CollectorNumber:
             matching_collector_numbers = source_model.card_db.find_collector_numbers_matching(
                 index.siblingAtColumn(PageColumns.CardName).data(Qt.EditRole),
                 index.siblingAtColumn(PageColumns.Set).data(Qt.EditRole),
@@ -64,14 +65,16 @@ class ComboBoxItemDelegate(QStyledItemDelegate):
             )
             for collector_number in matching_collector_numbers:
                 editor.addItem(collector_number, collector_number)  # Store the key in the UserData role
-            editor.setCurrentIndex(matching_collector_numbers.index(index.data(Qt.EditRole)))
+            if matching_collector_numbers:
+                editor.setCurrentIndex(matching_collector_numbers.index(index.data(Qt.EditRole)))
 
-        elif index.column() == PageColumns.Language:
+        elif column == PageColumns.Language:
             card = index.data(Qt.UserRole)
             matching_languages = source_model.card_db.get_available_languages_for_card(card)
             for language in matching_languages:
                 editor.addItem(language, language)
-            editor.setCurrentIndex(matching_languages.index(index.data(Qt.EditRole)))
+            if matching_languages:
+                editor.setCurrentIndex(matching_languages.index(index.data(Qt.EditRole)))
 
     def setModelData(self, editor: QComboBox, model: QAbstractItemModel, index: QModelIndex) -> None:
         new_value = editor.currentData(Qt.UserRole)
