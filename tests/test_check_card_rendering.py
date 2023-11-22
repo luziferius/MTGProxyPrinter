16d859492abc17028f10fd4208663d3ef287aa66
thomas <>
2023-11-22 12:22:44 +0000

    test_check_card_rendering.py no longer uses the ImageDatabase fixture for the blank image. The tests now use a minimal fixture that only provides that pixmap.
    FossilOrigin-Name: f27eb56017145befa34ca5d7a85ad3c3f3cef4b14df04d94f81717b1fcd923cd

--- tests/test_check_card_rendering.py
+++ tests/test_check_card_rendering.py
@@ -16,39 +16,47 @@
 import pytest
 from hamcrest import *
 from pytestqt.qtbot import QtBot
+from PyQt5.QtGui import QPixmap, QColorConstants
 
 from mtg_proxy_printer.model.carddb import Card, CheckCard, MTGSet
-from mtg_proxy_printer.model.imagedb import ImageDatabase
+from mtg_proxy_printer.model.imagedb import IMAGE_SIZE
 
 
-def test_render_check_card_image_is_not_none_if_both_faces_have_an_image(qtbot, image_db: ImageDatabase):
+@pytest.fixture
+def blank_image(qtbot) -> QPixmap:
+    pixmap = QPixmap(IMAGE_SIZE)
+    pixmap.fill(QColorConstants.White)
+    return pixmap
+
+
+def test_render_check_card_image_is_not_none_if_both_faces_have_an_image(blank_image: QPixmap):
     front = Card(name='Search for Azcanta', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=True, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/front/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=0, is_dfc=True,
-                 image_file=image_db.blank_image)
+                 image_file=blank_image)
     back = Card(name='Azcanta, the Sunken Ruin', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=False, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/back/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=1, is_dfc=True,
-                image_file=image_db.blank_image)
+                image_file=blank_image)
     check_card = CheckCard(front, back)
     assert_that(check_card.image_file, is_(not_none()))
 
 
-def test_render_check_card_image_is_none_if_front_image_is_None(qtbot, image_db: ImageDatabase):
+def test_render_check_card_image_is_none_if_front_image_is_None(blank_image: QPixmap):
     front = Card(name='Search for Azcanta', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=True, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/front/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=0, is_dfc=True,
                  image_file=None)
     back = Card(name='Azcanta, the Sunken Ruin', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=False, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/back/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=1, is_dfc=True,
-                image_file=image_db.blank_image)
+                image_file=blank_image)
     check_card = CheckCard(front, back)
     assert_that(check_card.image_file, is_(none()))
 
 
-def test_render_check_card_image_is_none_if_back_image_is_None(qtbot, image_db: ImageDatabase):
+def test_render_check_card_image_is_none_if_back_image_is_None(blank_image: QPixmap):
     front = Card(name='Search for Azcanta', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=True, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/front/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=0, is_dfc=True,
-                 image_file=image_db.blank_image)
+                 image_file=blank_image)
     back = Card(name='Azcanta, the Sunken Ruin', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=False, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/back/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=1, is_dfc=True,
                 image_file=None)
     check_card = CheckCard(front, back)
     assert_that(check_card.image_file, is_(none()))
 
 
-def test_render_check_card_image_is_none_if_both_images_are_None(qtbot):
+def test_render_check_card_image_is_none_if_both_images_are_None():
     front = Card(name='Search for Azcanta', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=True, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/front/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=0, is_dfc=True,
                  image_file=None)
     back = Card(name='Azcanta, the Sunken Ruin', set=MTGSet(code='xln', name='Ixalan'), collector_number='74', language='en', scryfall_id='1a7e242e-bb48-4134-a1c2-6033713d658f', is_front=False, oracle_id='f74c4d96-bc4a-4d32-9519-a753d192144e', image_uri='https://cards.scryfall.io/png/back/1/a/1a7e242e-bb48-4134-a1c2-6033713d658f.png?1562551479', highres_image=True, is_oversized=False, face_number=1, is_dfc=True,
@@ -58,7 +66,7 @@ def test_render_check_card_image_is_none_if_both_images_are_None(qtbot):
 
 
 @pytest.mark.skip("Skipping check card render test, as it hits the network and requires manual result inspection")
-def test_render_check_card(qtbot: QtBot, image_db: ImageDatabase):
+def test_render_check_card(qtbot: QtBot, image_db):
     """
     This test pulls real card images from the Scryfall download server and renders a check card to a file in the current
     working directory.
