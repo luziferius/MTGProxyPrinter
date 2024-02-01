2950507ef22fde832c2f2d50cd46617d09b0da69
thomas <>
2024-02-01 16:17:27 +0000

    Revert [30b5a1d484e22b], because it looks bad with borderless cards. Sampling closer to the image edge works better with those cards.
    FossilOrigin-Name: 874207484b5a1028e8f1b07c26931aa900bde4c81c5d78910f61c1a26ca7d92e

--- mtg_proxy_printer/ui/page_scene.py
+++ mtg_proxy_printer/ui/page_scene.py
@@ -134,10 +134,10 @@ class CardItem(QGraphicsItemGroup):
         h_size = QSize(width, 1)
         v_size = QSize(1, height)
         bleeds = CardBleeds(
-            self._create_bleed(pixmap, transformations.top, QRect(QPoint(0, 2), h_size)),
-            self._create_bleed(pixmap, transformations.bottom, QRect(QPoint(0, height - 2), h_size)),
-            self._create_bleed(pixmap, transformations.left, QRect(QPoint(2, 0), v_size)),
-            self._create_bleed(pixmap, transformations.right, QRect(QPoint(width - 2, 0), v_size))
+            self._create_bleed(pixmap, transformations.top, QRect(QPoint(0, 1), h_size)),
+            self._create_bleed(pixmap, transformations.bottom, QRect(QPoint(0, height - 1), h_size)),
+            self._create_bleed(pixmap, transformations.left, QRect(QPoint(1, 0), v_size)),
+            self._create_bleed(pixmap, transformations.right, QRect(QPoint(width - 1, 0), v_size))
         )
         bleeds.bottom.setPos(0, height)
         bleeds.right.setPos(width, 0)
