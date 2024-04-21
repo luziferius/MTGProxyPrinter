29945d60035d1cb884f297d7e6dec846462f821f
thomas <>
2024-04-21 18:28:31 +0000

    Pull Ui compiler improvements and class name / ui class name alignment improvements from branch [port_pyside6].
    FossilOrigin-Name: 55b69bba3a70f074418f556124dbe5415cf85f90697904cbe4c589b4ddb6dcea

--- mtg_proxy_printer/ui/add_card.py
+++ mtg_proxy_printer/ui/add_card.py
@@ -31,11 +31,11 @@ logger = get_logger(__name__)
 del get_logger
 
 try:
-    from mtg_proxy_printer.ui.generated.add_card_widget.vertical import Ui_AddCardWidget_Vertical
-    from mtg_proxy_printer.ui.generated.add_card_widget.horizontal import Ui_AddCardWidget_Horizontal
+    from mtg_proxy_printer.ui.generated.add_card_widget.vertical import Ui_VerticalAddCardWidget
+    from mtg_proxy_printer.ui.generated.add_card_widget.horizontal import Ui_HorizontalAddCardWidget
 except ModuleNotFoundError:
-    Ui_AddCardWidget_Vertical = load_ui_from_file("add_card_widget/vertical")
-    Ui_AddCardWidget_Horizontal = load_ui_from_file("add_card_widget/horizontal")
+    Ui_VerticalAddCardWidget = load_ui_from_file("add_card_widget/vertical")
+    Ui_HorizontalAddCardWidget = load_ui_from_file("add_card_widget/horizontal")
 
 
 __all__ = [
@@ -44,7 +44,7 @@ __all__ = [
     "HorizontalAddCardWidget",
 ]
 
-UiTypes = Union[Type[Ui_AddCardWidget_Vertical], Type[Ui_AddCardWidget_Horizontal]]
+UiTypes = Union[Type[Ui_VerticalAddCardWidget], Type[Ui_HorizontalAddCardWidget]]
 StandardButton = QDialogButtonBox.StandardButton
 ItemDataRole = Qt.ItemDataRole
 
@@ -282,9 +282,9 @@ class AddCardWidget(QWidget):
 class VerticalAddCardWidget(AddCardWidget):
 
     def __init__(self, parent: QWidget = None):
-        super().__init__(Ui_AddCardWidget_Vertical, parent)
+        super().__init__(Ui_VerticalAddCardWidget, parent)
 
 
 class HorizontalAddCardWidget(AddCardWidget):
     def __init__(self, parent: QWidget = None):
-        super().__init__(Ui_AddCardWidget_Horizontal, parent)
+        super().__init__(Ui_HorizontalAddCardWidget, parent)
