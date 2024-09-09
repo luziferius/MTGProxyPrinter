5349ec6dc57e84a927e2e26d22682fb5ca14bcf9
thomas <>
2024-09-09 16:39:00 +0000

    WIP file extension registration for Windows cx_Freeze bundles.
    FossilOrigin-Name: bc8c8386027d946e6f2cd086ca943c85a301defb7c728246020ad2880f4bfeb3

--- setup_cx_freeze.py
+++ setup_cx_freeze.py
@@ -116,6 +116,14 @@ setup_parameters = {
             # causing the removal of the previous package, allowing smooth updates. It has to be enclosed in {}.
             "upgrade_code": "{15a9e385-f6ab-4aa4-8ef1-3f2cf5c193a8}",
             "target_name": project_name,
+            "extensions": [{
+                "extension": "mtgproxies",
+                "verb": "open",  # TODO: l10n?
+                "context": f"Open with {project_name}",  # TODO: l10n?
+                "executable": f"{project_name}.exe",
+                "argument": '%1',
+                },
+            ],
             "skip_build": True,
         },
         "build_exe": {
