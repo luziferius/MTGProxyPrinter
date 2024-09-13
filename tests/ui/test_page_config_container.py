4bf3d2509d5bebe6cf8172c24f3f4a266b6897f7
thomas <>
2024-09-13 12:19:11 +0000

    tests/ui/test_page_config_container.py: Fixed bad import
    FossilOrigin-Name: 0f75057af04dca4af882f92690a4813d596a36b90dba1a3b998187c3ff13ad73

--- tests/ui/test_page_config_container.py
+++ tests/ui/test_page_config_container.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from PySide6.QtWidgets import QCheckBox, QDoubleSpinBox, QLineEdit
+from PyQt5.QtWidgets import QCheckBox, QDoubleSpinBox, QLineEdit
 
 import pytest
 from pytestqt.qtbot import QtBot
