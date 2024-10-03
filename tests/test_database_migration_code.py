eeb3724e53de0bf9c27d42e340d8ad09c0de6b7e
thomas <>
2024-10-03 13:47:33 +0000

    Partial fix for the database migration tests.
    FossilOrigin-Name: dcb11ec4b703e58c1e64137f7e8d02518d8c2dc2666932dd921ab636925ceb51

--- tests/test_database_migration_code.py
+++ tests/test_database_migration_code.py
@@ -14,6 +14,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+import pathlib
 import socket
 import sqlite3
 import textwrap
@@ -23,9 +24,10 @@ import urllib.error
 from hamcrest import *
 import pytest
 
-from mtg_proxy_printer.sqlite_helpers import _get_target_database_schema_version
+import mtg_proxy_printer.sqlite_helpers
+from mtg_proxy_printer.sqlite_helpers import get_target_database_schema_version
 from mtg_proxy_printer.model.carddb import CardDatabase
-from mtg_proxy_printer.carddb_migrations import migrate_card_database, MIGRATION_SCRIPTS, _migrate_21_to_22
+from mtg_proxy_printer.carddb_migrations import DatabaseMigrationRunner, MIGRATION_SCRIPTS, Migrate_21_to_22
 
 # Pulled from check-in [43d8e4f754efc85d7f52ce9f8c87e93a6ed31e39de862a44a18d28b0590c113c].
 # NOTE: Removed all SQL comments present in the original file.
@@ -83,15 +85,23 @@ CREATE VIEW AllPrintings AS
 COMMIT;
 """
 
+@pytest.fixture()
+def old_db(tmp_path: pathlib.Path) -> pathlib.Path:
+    target_path = tmp_path/"CardDatabase.sqlite3"
+    db = sqlite3.Connection(target_path)
+    db.executescript(OLDEST_SUPPORTED_SCHEMA)
+    return target_path
 
-def test_migrated_card_database_contains_expected_tables_and_views(card_db: CardDatabase):
-    migrated_db = sqlite3.connect(":memory:")
-    fresh_db = card_db.db
-    migrated_db.executescript(OLDEST_SUPPORTED_SCHEMA)
-    migrate_card_database(migrated_db)
+
+def test_migrated_card_database_contains_expected_tables_and_views(old_db: pathlib.Path):
+    card_db = CardDatabase(old_db)
+    runner = DatabaseMigrationRunner(card_db)
+    runner.run()
+    migrated_db = card_db.db
+    fresh_db = mtg_proxy_printer.sqlite_helpers.open_database(":memory:", "carddb", CardDatabase.MIN_SUPPORTED_SQLITE_VERSION)
     assert_that(
         migrated_db.execute("PRAGMA user_version").fetchone()[0],
-        is_(equal_to(_get_target_database_schema_version("carddb"))),
+        is_(equal_to(get_target_database_schema_version("carddb"))),
     )
     # Query the table and view definitions.
     tables_and_views_query = textwrap.dedent("""\
@@ -114,14 +124,15 @@ def test_migrated_card_database_contains_expected_tables_and_views(card_db: Card
         ))
 
 
-def test_migrated_card_database_contains_expected_indices(card_db: CardDatabase):
-    migrated_db = sqlite3.connect(":memory:")
-    fresh_db = card_db.db
-    migrated_db.executescript(OLDEST_SUPPORTED_SCHEMA)
-    migrate_card_database(migrated_db)
+def test_migrated_card_database_contains_expected_indices(old_db: pathlib.Path):
+    card_db = CardDatabase(old_db)
+    runner = DatabaseMigrationRunner(card_db)
+    runner.run()
+    migrated_db = card_db.db
+    fresh_db = mtg_proxy_printer.sqlite_helpers.open_database(":memory:", "carddb", CardDatabase.MIN_SUPPORTED_SQLITE_VERSION)
     assert_that(
         migrated_db.execute("PRAGMA user_version").fetchone()[0],
-        is_(equal_to(_get_target_database_schema_version("carddb"))),
+        is_(equal_to(get_target_database_schema_version("carddb"))),
     )
     # Note: Also include the “sqlite_autoindex*” indices that are
     # automatically created for UNIQUE and PRIMARY KEY constraints.
@@ -143,49 +154,47 @@ def test_migrated_card_database_contains_expected_indices(card_db: CardDatabase)
 
 
 @pytest.fixture
-def card_db_at_version_21() -> sqlite3.Connection:
-    previous_patches = MIGRATION_SCRIPTS[:MIGRATION_SCRIPTS.index((21, _migrate_21_to_22))]
-    db = sqlite3.connect(":memory:")
-    db.executescript(OLDEST_SUPPORTED_SCHEMA)
-    migrate_card_database(db, previous_patches)
-    assert_that(db.execute("PRAGMA user_version").fetchone()[0], is_(equal_to(21)))
+def card_db_at_version_21(old_db) -> pathlib.Path:
+    card_db = CardDatabase(old_db)
+    runner = DatabaseMigrationRunner(card_db, {src: script for src, script in MIGRATION_SCRIPTS.items() if src < 21})
+    runner.run()
+    db = card_db.db
+    assert_that(db.execute("PRAGMA user_version").fetchone()[0], is_(equal_to(21)), "Setup failed")
     today_tuple = str(datetime.date.today()),
     db.execute("INSERT INTO LastDatabaseUpdate (newest_card_timestamp) values (?)", today_tuple)
     db.commit()
-    return db
+    return old_db
 
 
 @pytest.mark.parametrize("possible_error", [urllib.error.URLError("Test case"), socket.error()])
 def test_patch_21_to_22_applies_correctly_without_network_access_using_dummy_values(
-        card_db_at_version_21: sqlite3.Connection, possible_error: BaseException):
+        card_db_at_version_21: pathlib.Path, possible_error: BaseException):
+    card_db = CardDatabase(card_db_at_version_21)
     with unittest.mock.patch(
             "mtg_proxy_printer.card_info_downloader.CardInfoDatabaseImportWorker.read_json_card_data_from_url") as mock:
         mock.side_effect = possible_error
-        migrate_card_database(
-            card_db_at_version_21,
-            ((21, _migrate_21_to_22),)
-        )
-        assert_that(card_db_at_version_21.execute("PRAGMA user_version").fetchone()[0], is_(equal_to(22)))
+        runner = DatabaseMigrationRunner(card_db)
+        runner.run({21: MIGRATION_SCRIPTS[21]})
+        assert_that(card_db.db.execute("PRAGMA user_version").fetchone()[0], is_(equal_to(22)))
         mock.assert_called_once()
     assert_that(
-        card_db_at_version_21.execute("SELECT MAX(update_id), reported_card_count FROM LastDatabaseUpdate").fetchall(),
+        card_db.db.execute("SELECT MAX(update_id), reported_card_count FROM LastDatabaseUpdate").fetchall(),
         contains_exactly((1, 0))
     )
 
 
 @pytest.mark.parametrize("expected", [1, 300000])
 def test_patch_21_to_22_applies_with_network_access_and_requests_card_count_from_api(
-        card_db_at_version_21: sqlite3.Connection, expected):
+        card_db_at_version_21: pathlib.Path, expected):
+    card_db = CardDatabase(card_db_at_version_21)
     with unittest.mock.patch(
             "mtg_proxy_printer.card_info_downloader.CardInfoDatabaseImportWorker.read_json_card_data_from_url") as mock:
         mock.return_value = iter((expected,))
-        migrate_card_database(
-            card_db_at_version_21,
-            ((21, _migrate_21_to_22),)
-        )
-        assert_that(card_db_at_version_21.execute("PRAGMA user_version").fetchone()[0], is_(equal_to(22)))
+        runner = DatabaseMigrationRunner(card_db)
+        runner.run({21: MIGRATION_SCRIPTS[21]})
+        assert_that(card_db.db.execute("PRAGMA user_version").fetchone()[0], is_(equal_to(22)))
         mock.assert_called_once()
     assert_that(
-        card_db_at_version_21.execute("SELECT MAX(update_id), reported_card_count FROM LastDatabaseUpdate").fetchall(),
+        card_db.db.execute("SELECT MAX(update_id), reported_card_count FROM LastDatabaseUpdate").fetchall(),
         contains_exactly((1, expected))
     )
