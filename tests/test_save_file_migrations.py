5c1923fe4761c5d41e4ca11a03585db1ec9ed76f
thomas <>
2025-03-22 12:52:45 +0000

    Implement test for _migrate_5_to_6(). Cleaned up imports.
    FossilOrigin-Name: 516971ed1482c993f8ff34a58d9fce7edd22013e4d2fee4749d006c332924a50

--- tests/test_save_file_migrations.py
+++ tests/test_save_file_migrations.py
@@ -13,35 +13,17 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
-import contextlib
-import itertools
-import functools
-import pathlib
 import sqlite3
-import typing
-import unittest.mock
-import textwrap
 from typing import Callable
 
-import pint
-from pint.facets import QuantityT
-from pytestqt.qtbot import QtBot
 import pytest
 from hamcrest import *
 
 import mtg_proxy_printer.model.document_loader
 from mtg_proxy_printer.model.page_layout import PageLayoutSettings
-from tests.helpers import quantity_close_to
-from mtg_proxy_printer.units_and_sizes import PageType, unit_registry, UnitT
-from mtg_proxy_printer.model.carddb import CheckCard
 import mtg_proxy_printer.model.document
-
 import mtg_proxy_printer.sqlite_helpers
 from mtg_proxy_printer.model.document_loader import SAVE_FILE_MAGIC_NUMBER, DocumentLoader
-
-from mtg_proxy_printer.save_file_migrations import migrate_database, _migrate_2_to_3, _migrate_3_to_4, _migrate_4_to_5,\
-    _migrate_5_to_6, _migrate_image_spacing_settings
 import mtg_proxy_printer.save_file_migrations
 
 
@@ -103,12 +85,16 @@ def test_migration_4_to_5_transforms_data():
     settings.draw_sharp_corners = True
     cards = [(1, 1, 1, 'abc'), (2, 1, 1, 'xyz')]
     db.executemany("INSERT INTO CARD VALUES (?, ?, ?, ?)", cards)
-    db.execute("INSERT INTO DocumentSettings VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (1, settings.page_height.to("mm").magnitude, settings.page_width.to("mm").magnitude,
-         settings.margin_top.to("mm").magnitude, settings.margin_bottom.to("mm").magnitude,
+    # Insert slightly altered data, then pass the unaltered PageLayoutSettings.
+    # This verifies that the stored data is used and not replaced with the current default settings.
+    db.execute(
+        "INSERT INTO DocumentSettings VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        (1, settings.page_height.to("mm").magnitude, settings.page_width.to("mm").magnitude,
+         settings.margin_top.to("mm").magnitude+1, settings.margin_bottom.to("mm").magnitude-1,
          settings.margin_left.to("mm").magnitude, settings.margin_right.to("mm").magnitude,
          settings.row_spacing.to("mm").magnitude, settings.column_spacing.to("mm").magnitude,
-         int(settings.draw_cut_markers))
-               )
+         int(not settings.draw_cut_markers))
+    )
     mtg_proxy_printer.save_file_migrations._migrate_4_to_5(db, settings)
     assert_that(
         db.execute("SELECT * FROM Card ORDER BY page ASC, slot ASC").fetchall(),
@@ -118,9 +104,43 @@ def test_migration_4_to_5_transforms_data():
         db.execute("SELECT * FROM DocumentSettings").fetchall(),
         contains_exactly(
         (1, settings.page_height.to("mm").magnitude, settings.page_width.to("mm").magnitude,
-         settings.margin_top.to("mm").magnitude, settings.margin_bottom.to("mm").magnitude,
+         settings.margin_top.to("mm").magnitude+1, settings.margin_bottom.to("mm").magnitude-1,
          settings.margin_left.to("mm").magnitude, settings.margin_right.to("mm").magnitude,
          settings.row_spacing.to("mm").magnitude, settings.column_spacing.to("mm").magnitude,
-         int(settings.draw_cut_markers), int(settings.draw_sharp_corners)
+         int(not settings.draw_cut_markers), int(settings.draw_sharp_corners)
          )
+    ))
+
+def test_migration_5_to_6_transforms_data():
+    db = create_save_db(5)
+    settings = PageLayoutSettings.create_from_settings()
+    settings.draw_sharp_corners = True
+    db.executemany("INSERT INTO CARD VALUES (?, ?, ?, ?)", [(1, 1, 1, 'abc'), (2, 1, 1, 'xyz')])
+    # Insert slightly altered data, then pass the unaltered PageLayoutSettings.
+    # This verifies that the stored data is used and not replaced with the current default settings.
+    db.execute(
+        "INSERT INTO DocumentSettings VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        (1, settings.page_height.to("mm").magnitude, settings.page_width.to("mm").magnitude,
+         settings.margin_top.to("mm").magnitude+1, settings.margin_bottom.to("mm").magnitude-1,
+         settings.margin_left.to("mm").magnitude, settings.margin_right.to("mm").magnitude,
+         settings.row_spacing.to("mm").magnitude, settings.column_spacing.to("mm").magnitude,
+         int(not settings.draw_cut_markers), int(not settings.draw_sharp_corners))
+    )
+    mtg_proxy_printer.save_file_migrations._migrate_5_to_6(db, settings)
+    assert_that(
+        db.execute("SELECT * FROM Card ORDER BY page ASC, slot ASC").fetchall(),
+        contains_exactly((1, 1, 1, 'abc', 'r'), (2, 1, 1, 'xyz', 'r'))
+    )
+    assert_that(
+        db.execute("SELECT * FROM DocumentSettings").fetchall(),
+        contains_inanyorder(
+            # Migrated
+            ("page_height", settings.page_height.to("mm").magnitude), ("page_width", settings.page_width.to("mm").magnitude),
+            ("margin_top", settings.margin_top.to("mm").magnitude+1),("margin_bottom", settings.margin_bottom.to("mm").magnitude-1),
+            ("margin_left", settings.margin_left.to("mm").magnitude),("margin_right", settings.margin_right.to("mm").magnitude),
+            ("row_spacing", settings.row_spacing.to("mm").magnitude),("column_spacing", settings.column_spacing.to("mm").magnitude),
+            ("draw_cut_markers", int(not settings.draw_cut_markers)),("draw_sharp_corners", int(not settings.draw_sharp_corners)),
+            # New
+            ("document_name", settings.document_name), ("card_bleed", settings.card_bleed.to("mm").magnitude),
+            ("draw_page_numbers", int(settings.draw_page_numbers)),
     ))
