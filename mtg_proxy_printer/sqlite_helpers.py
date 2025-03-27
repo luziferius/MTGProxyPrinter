eaa21b74d32247987e0f98f8e6ccf76b2a54c4eb
thomas <>
2025-03-27 14:26:18 +0000

    Fix the registered adapter for TIMESTAMP. Fixes Python warning.
    FossilOrigin-Name: 8ac57a45c578f9f7659cdcd61009090994cbed352f2b1389a76d81e2b3652d51

--- mtg_proxy_printer/sqlite_helpers.py
+++ mtg_proxy_printer/sqlite_helpers.py
@@ -46,7 +46,7 @@ sqlite3.register_adapter(pathlib.WindowsPath, str)
 sqlite3.register_adapter(type(1*unit_registry.mm), str)
 sqlite3.register_converter("TEXT_QUANTITY", lambda b: unit_registry.parse_expression(b.decode("utf-8")))
 sqlite3.register_converter("BOOLEAN_INTEGER", lambda b: bool(int(b)))
-sqlite3.register_converter("TIMESTAMP WITH TIME ZONE", lambda b: datetime.datetime.fromisoformat(b.decode("utf-8")))
+sqlite3.register_converter("TIMESTAMP", lambda b: datetime.datetime.fromisoformat(b.decode("utf-8")))
 
 
 def read_resource_text(package: str, resource: str, encoding: str = "utf-8") -> str:
