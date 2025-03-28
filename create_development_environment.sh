691ff6c90a303afdb6dbbdb2c17beb93553e322f
thomas <>
2025-03-28 12:56:44 +0000

    Dev environment: create_development_environment scripts now echo that creating requirements.txt will take a while before generating them.
    FossilOrigin-Name: 5ac44455657e951426bdfc0d107a4164777004aaf84194db6cfa43415e9e435a

--- create_development_environment.sh
+++ create_development_environment.sh
@@ -15,6 +15,7 @@ echo "Installing all dependencies, including development and test requirements."
 python -m pip install --upgrade pip
 python -m pip install wheel
 python -m pip install "pip-tools >= 7"
+echo "Creating requirements.txt from pyproject.toml. This takes a while."
 python -m piptools compile -o requirements.txt pyproject.toml
 python -m piptools compile --extra dev -o requirements-dev.txt pyproject.toml
 python -m piptools compile --extra package -o requirements-package.txt pyproject.toml
