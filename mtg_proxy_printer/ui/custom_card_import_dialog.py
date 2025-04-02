9a7e88730162a36538b1a2ddbfe98e48caca7271
thomas <>
2025-04-02 22:24:42 +0000

    Adjusted internals of the CustomCardImportDialog. It now has an actual model class to hold the cards, and displays them via the connected table view.
    FossilOrigin-Name: f3099f5e2d33078eb0823f3a4a4309c664a7925675d712df906ebefef4e23bc8

--- mtg_proxy_printer/ui/custom_card_import_dialog.py
+++ mtg_proxy_printer/ui/custom_card_import_dialog.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+from collections import Counter
 from pathlib import Path
 import typing
 
@@ -20,12 +20,16 @@ from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QPixmap
 from PyQt5.QtWidgets import QDialog, QWidget
 
+from mtg_proxy_printer.model.carddb import CardDatabase, Card, MTGSet
+from mtg_proxy_printer.units_and_sizes import CardSizes
+
 try:
     from mtg_proxy_printer.ui.generated.custom_card_import_dialog import Ui_CustomCardImportDialog
 except ModuleNotFoundError:
     from mtg_proxy_printer.ui.common import load_ui_from_file
     Ui_CustomCardImportDialog = load_ui_from_file("custom_card_import_dialog")
 
+from mtg_proxy_printer.model.card_list import CardListModel  # TODO: This doesn't fit. Doesn't (yet) support editing custom cards
 import mtg_proxy_printer.units_and_sizes
 from mtg_proxy_printer.logger import get_logger
 logger = get_logger(__name__)
@@ -36,10 +40,12 @@ EventTypes = typing.Union[QDragEnterEvent, QDropEvent]
 
 class CustomCardImportDialog(QDialog):
 
-    def __init__(self, parent: QWidget = None, flags=Qt.WindowFlags()):
+    def __init__(self, card_db: CardDatabase, parent: QWidget = None, flags=Qt.WindowFlags()):
         super().__init__(parent, flags)
         self.ui = ui = Ui_CustomCardImportDialog()
         ui.setupUi(self)
+        self.model = CardListModel(card_db)
+        ui.image_table.setModel(self.model)  # TODO: The table needs validators for the copies column
 
     @staticmethod
     def dragdrop_acceptable(event: EventTypes) -> bool:
@@ -47,16 +53,25 @@ class CustomCardImportDialog(QDialog):
         local_paths = [Path(url.toLocalFile()) for url in urls]
         return local_paths and all((path.is_file() for path in local_paths))
 
+    def show_from_drop_event(self, event: QDropEvent):
+        urls = event.mimeData().urls()
+        local_paths = [Path(url.toLocalFile()) for url in urls]
+        cards = self.create_cards(local_paths)
+        self.model.add_cards(cards)
+        self.show()
+
     @staticmethod
-    def populate_model(event: QDropEvent):
-        result: typing.List[QPixmap] = []
-        mime_data = event.mimeData()
+    def create_cards(paths: typing.List[Path]) -> typing.Counter[Card]:
+        result: typing.Counter[Card] = Counter()
         regular = mtg_proxy_printer.units_and_sizes.CardSizes.REGULAR
         width, height = regular.width.magnitude, regular.height.magnitude
-        for url in mime_data.urls():
-            pixmap = QPixmap(url.toLocalFile())
+        for path in paths:
+            pixmap = QPixmap(str(path))
             if not pixmap.isNull():
                 if pixmap.width() != width or pixmap.height() != height:
                     new_size = QSize(width, height)
                     pixmap = pixmap.scaled(new_size, transformMode=TransformationMode.SmoothTransformation)
-                result.append(pixmap)
+                # TODO: The object hash has to take the URI or pixmap into account
+                card = Card("Custom card", MTGSet("", ""), "", "en", "", True, "", str(path), True, regular, 1, False, pixmap)
+                result[card] += 1
+        return result
