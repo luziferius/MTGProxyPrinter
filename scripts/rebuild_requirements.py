331c5e9bfccdfdcf3f920b7ab3ca7d90cb5376f2
thomas <>
2025-04-26 11:54:40 +0000

    Scripts: Refactored rebuild_requirements.py to not use map, instead use ThreadPoolExecutor.submit() and wait()
    FossilOrigin-Name: dd26849b3f9f33800e17731fba49929948dd695a943ab2b0d164247457a1cbcb

--- scripts/rebuild_requirements.py
+++ scripts/rebuild_requirements.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 from subprocess import call
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait
 
 repo_root = Path(__file__).parent.parent
 source = repo_root / "pyproject.toml"
@@ -10,14 +10,12 @@ req = repo_root / "requirements.txt"
 req_dev = repo_root / "requirements-dev.txt"
 req_pack = repo_root / "requirements-package.txt"
 
-call_params = [
+tasks = [
     ["python", "-m", "piptools", "compile", "-o", req, source],
     ["python", "-m", "piptools", "compile", "--extra", "dev", "-o", req_dev, source],
     ["python", "-m", "piptools", "compile", "--extra", "package", "-o", req_pack, source],
 ]
 
 if __name__ == "__main__":
-    pool = ThreadPoolExecutor()
-    for _ in pool.map(call, call_params):
-        pass
-    pool.shutdown()
+    with ThreadPoolExecutor() as pool:
+        wait(pool.submit(call, args) for args in tasks)
