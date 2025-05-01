2512deffac67d61ffd112317ad1841eb4d87bb7b
thomas <>
2025-05-01 19:18:19 +0000

    Release v0.31Â.0
    FossilOrigin-Name: 2fae13d0916d610359ee705e982942a7f8f3068368ebcb6f392297813fec1e7d

--- mtg_proxy_printer/meta_data.py
+++ mtg_proxy_printer/meta_data.py
@@ -15,7 +15,7 @@
 
 
 PROGRAMNAME = "MTGProxyPrinter"
-__version__ = "0.30.1"
+__version__ = "0.31.0"
 COPYRIGHT = "(C) 2020-2025 Thomas Hess"
 HOME_PAGE = "https://chiselapp.com/user/luziferius/repository/MTGProxyPrinter"
 
