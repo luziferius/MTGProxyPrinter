1cf746d79de72b89fc0c8d9420a82030c438f698
thomas <>
2025-05-07 09:18:07 +0000

    Remove all <normaloff> elements from UI files. Add test module verifying that none are present across all UI files.
    FossilOrigin-Name: 2b34942f16d6e53fb7881202fdd8e312eded77954aca2c3e9049399fc89fbd15

---
+++ tests/ui/test_ui_files.py
@@ -0,0 +1,36 @@
+#  Copyright © 2020-2025  Thomas Hess <thomas.hess@udo.edu>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+from xml.etree import ElementTree
+
+import pytest
+from hamcrest import *
+
+
+from mtg_proxy_printer.ui.common import RESOURCE_PATH_PREFIX, HAS_COMPILED_RESOURCES
+
+pytest.mark.skipif(
+    HAS_COMPILED_RESOURCES, reason="Cannot test UI files in compiled mode, as they are unavailable"
+)
+
+
+@pytest.mark.parametrize("ui_file_path", (Path(RESOURCE_PATH_PREFIX)/"ui").rglob("*.ui"))
+def test_button_icons_do_not_have_normaloff_value(ui_file_path: Path):
+    tree = ElementTree.parse(ui_file_path)
+    assert_that(
+        (items := tree.findall(".//normaloff/..")),
+        is_(empty()),
+        f"Affected icons: {', '.join(item.attrib['theme'] for item in items)}")
