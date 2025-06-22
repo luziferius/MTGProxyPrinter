ec3306c5d133bae44a4ed825b828db6e9e460802
thomas <>
2025-06-22 09:23:23 +0000

    tests/document_controller/test_action_add_card.py: Add type hints to methods.
    FossilOrigin-Name: 32dfed840de11dd484dd1a47c9cfb46cc4a5e70b106b4c5a5c0e494b61a718eb

--- tests/document_controller/test_action_add_card.py
+++ tests/document_controller/test_action_add_card.py
@@ -19,9 +19,11 @@ import unittest.mock
 
 import pytest
 from hamcrest import *
+from pytestqt.qtbot import QtBot
 
 from mtg_proxy_printer.model.card import MTGSet, Card, CheckCard
 from mtg_proxy_printer.model.document_page import PageType
+from mtg_proxy_printer.model.document import Document
 from mtg_proxy_printer.model.imagedb import ImageDatabase
 from mtg_proxy_printer.document_controller import IllegalStateError
 from mtg_proxy_printer.document_controller.card_actions import ActionAddCard
@@ -33,17 +35,17 @@ from .helpers import insert_card_in_page, card_container_with
 
 
 @pytest.fixture()
-def card():
+def card() -> Card:
     return Card("", MTGSet("", ""), "", "", "", True, "", "", True, CardSizes.REGULAR, 0, False, None)
 
 
 @pytest.fixture()
-def oversized_card():
+def oversized_card() -> Card:
     return Card("", MTGSet("", ""), "", "", "", True, "", "", True, CardSizes.OVERSIZED, 0, False, None)
 
 
 @pytest.fixture()
-def check_card(card, image_db: ImageDatabase):
+def check_card(card: Card, image_db: ImageDatabase) -> CheckCard:
     card.image_file = image_db.get_blank()
     card.is_dfc = True
     other = copy.copy(card)
@@ -52,12 +54,12 @@ def check_card(card, image_db: ImageDatabase):
 
 
 @pytest.fixture()
-def document_light_3(document_light):
+def document_light_3(document_light: Document) -> Document:
     ActionNewPage(count=2).apply(document_light)
     return document_light
 
 
-def test_apply_without_count_adds_single_card(qtbot, card, document_light):
+def test_apply_without_count_adds_single_card(qtbot: QtBot, card: Card, document_light: Document):
     action = ActionAddCard(card)
     page = document_light.pages[0]
     assert_that(action.apply(document_light), is_(same_instance(action)))
@@ -73,7 +75,7 @@ def test_apply_without_count_adds_single_card(qtbot, card, document_light):
 
 @pytest.mark.parametrize("target_page", range(3))
 def test_apply_without_count_and_with_target_page_adds_single_card_to_that_page(
-        qtbot, card, document_light_3, target_page: int):
+        qtbot: QtBot, card: Card, document_light_3: Document, target_page: int):
     action = ActionAddCard(card, target_page=target_page)
     page = document_light_3.pages[target_page]
     assert_that(action.apply(document_light_3), is_(same_instance(action)))
@@ -87,7 +89,8 @@ def test_apply_without_count_and_with_target_page_adds_single_card_to_that_page(
     assert_that(action.added_new_pages, is_(0))
 
 
-def test_apply_with_check_card_adds_card_to_current_page(qtbot, card, check_card, document_light):
+def test_apply_with_check_card_adds_card_to_current_page(
+        qtbot: QtBot, card: Card, check_card: Card, document_light: Document):
     action1 = ActionAddCard(card)
     action2 = ActionAddCard(check_card)
     page = document_light.pages[0]
@@ -104,7 +107,7 @@ def test_apply_with_check_card_adds_card_to_current_page(qtbot, card, check_card
 
 
 @pytest.mark.parametrize("count", [1, 3, 9])
-def test_apply_with_count_adds_that_many_copies(qtbot, card, document_light, count: int):
+def test_apply_with_count_adds_that_many_copies(qtbot: QtBot, card: Card, document_light: Document, count: int):
     action = ActionAddCard(card, count)
     page = document_light.pages[0]
     assert_that(action.apply(document_light), is_(same_instance(action)))
@@ -121,7 +124,7 @@ def test_apply_with_count_adds_that_many_copies(qtbot, card, document_light, cou
 @pytest.mark.parametrize("count", [1, 3])
 @pytest.mark.parametrize("target_page", range(3))
 def test_apply_with_count_and_with_target_page_adds_that_many_copies_to_that_page(
-        qtbot, card, document_light_3, target_page: int, count: int):
+        qtbot: QtBot, card: Card, document_light_3: Document, target_page: int, count: int):
     action = ActionAddCard(card, count, target_page=target_page)
     page = document_light_3.pages[target_page]
     assert_that(action.apply(document_light_3), is_(same_instance(action)))
@@ -135,7 +138,8 @@ def test_apply_with_count_and_with_target_page_adds_that_many_copies_to_that_pag
     assert_that(action.added_new_pages, is_(0))
 
 
-def test_apply_with_target_page_works_while_currently_edited_page_is_full(qtbot, card, document_light_3):
+def test_apply_with_target_page_works_while_currently_edited_page_is_full(
+        qtbot: QtBot, card: Card, document_light_3: Document):
     document_light_3.set_currently_edited_page(document_light_3.pages[0])
     ActionAddCard(card, 9).apply(document_light_3)
     ActionAddCard(card, 2, target_page=1).apply(document_light_3)
@@ -149,7 +153,7 @@ def test_apply_with_target_page_works_while_currently_edited_page_is_full(qtbot,
     )
 
 
-def test_apply_with_count_overflowing_page_adds_new_page(qtbot, card, document_light):
+def test_apply_with_count_overflowing_page_adds_new_page(qtbot: QtBot, card: Card, document_light: Document):
     capacity = document_light.page_layout.compute_page_card_capacity()
     count = capacity * 3
     action = ActionAddCard(card, count)
@@ -165,7 +169,8 @@ def test_apply_with_count_overflowing_page_adds_new_page(qtbot, card, document_l
     assert_that(action.added_new_pages, is_(2))
 
 
-def test_apply_with_count_overflowing_page_adds_cards_to_next_page_with_empty_slots(qtbot, card, document_light_3):
+def test_apply_with_count_overflowing_page_adds_cards_to_next_page_with_empty_slots(
+        qtbot: QtBot, card: Card, document_light_3: Document):
     capacity = document_light_3.page_layout.compute_page_card_capacity()
     count = capacity + 1
     pages = document_light_3.pages
@@ -187,19 +192,19 @@ def test_apply_with_count_overflowing_page_adds_cards_to_next_page_with_empty_sl
     assert_that(action.added_new_pages, is_(0))
 
 
-def test_apply_emits_page_type_changed_when_page_type_changes(qtbot, card, document_light):
-    with qtbot.wait_signal(document_light.page_type_changed, timeout=1000, check_params_cb=lambda x: x.row() == 0):
+def test_apply_emits_page_type_changed_when_page_type_changes(qtbot: QtBot, card: Card, document_light: Document):
+    with qtbot.wait_signal(document_light.page_type_changed, check_params_cb=lambda x: x.row() == 0):
         ActionAddCard(card).apply(document_light)
     with qtbot.assert_not_emitted(document_light.page_type_changed):
         ActionAddCard(card).apply(document_light)
 
 
-def test_undo_without_internal_saved_state_raises_exception(card):
+def test_undo_without_internal_saved_state_raises_exception(card: Card):
     action = ActionAddCard(card)
     assert_that(calling(action.undo).with_args(unittest.mock.MagicMock), raises(IllegalStateError))
 
 
-def test_undo_deletes_pages_created_during_apply(qtbot, card, document_light):
+def test_undo_deletes_pages_created_during_apply(qtbot: QtBot, card: Card, document_light: Document):
     pages = document_light.pages
     capacity = document_light.page_layout.compute_page_card_capacity()
     append_new_pages(document_light, 2)
@@ -223,7 +228,7 @@ def test_undo_deletes_pages_created_during_apply(qtbot, card, document_light):
 
 @pytest.mark.parametrize("oversized_first", [True, False])
 def test_apply_does_not_create_pages_with_mixed_card_sizes(
-        document_light, card: Card, oversized_card: Card, oversized_first: bool):
+        document_light: Document, card: Card, oversized_card: Card, oversized_first: bool):
     if oversized_first:
         ActionAddCard(oversized_card).apply(document_light)
         ActionAddCard(card).apply(document_light)
@@ -237,7 +242,7 @@ def test_apply_does_not_create_pages_with_mixed_card_sizes(
         assert_that(page, has_length(1))
 
 
-def test_apply_does_not_overfill_oversized_pages(document_light, oversized_card: Card):
+def test_apply_does_not_overfill_oversized_pages(document_light: Document, oversized_card: Card):
     ActionAddCard(oversized_card, 7).apply(document_light)
     assert_that(document_light.rowCount(), is_(2))
     ActionAddCard(oversized_card, 2).apply(document_light)
@@ -246,7 +251,7 @@ def test_apply_does_not_overfill_oversized_pages(document_light, oversized_card:
 
 @pytest.mark.parametrize("page_type", [PageType.REGULAR, PageType.OVERSIZED])
 def test_apply_only_emits_page_type_changed_signal_if_changed(
-        qtbot, document_light, card, oversized_card, page_type: PageType):
+        qtbot: QtBot, document_light: Document, card: Card, oversized_card: Card, page_type: PageType):
     added_card = oversized_card if page_type == PageType.OVERSIZED else card
 
     with qtbot.wait_signal(document_light.page_type_changed):
@@ -256,7 +261,7 @@ def test_apply_only_emits_page_type_changed_signal_if_changed(
     assert_that(document_light.pages[0].page_type(), is_(page_type))
 
 
-def test_undo_works_when_apply_searched_over_multiple_full_pages_for_free_slot(document_light, card):
+def test_undo_works_when_apply_searched_over_multiple_full_pages_for_free_slot(document_light: Document, card: Card):
     """
     A ValueError (including a SegmentationFault) occurred in undo() when
     - apply() had to search over multiple full pages,
