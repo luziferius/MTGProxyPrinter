f3c65b6f193561da4d311f6e1d66c34714601f09
thomas <>
2025-07-11 08:59:22 +0000

    Remove unused and automatically added import that caused a cyclic import error
    FossilOrigin-Name: 6dc4787e7521860ddd7cda7a58be436266480d842a74f69b87ccf3b2351c8367

--- mtg_proxy_printer/model/document_loader.py
+++ mtg_proxy_printer/model/document_loader.py
@@ -28,8 +28,6 @@ from PyQt5.QtCore import QObject, pyqtSignal as Signal, QThreadPool, Qt
 from hamcrest import assert_that, all_of, instance_of, greater_than_or_equal_to, matches_regexp, is_in, \
     has_properties, is_, any_of, none, has_item, has_property, equal_to
 
-from tests.hasgetter import has_getter
-
 try:
     from hamcrest import contains_exactly
 except ImportError:
