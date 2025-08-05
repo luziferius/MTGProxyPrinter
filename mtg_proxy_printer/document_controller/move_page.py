2f6ac1a6f2ba1162d26af6789a7a36ad3c974048
thomas <>
2025-08-05 17:31:16 +0000

    Add WIP ActionMovePage, which will allow moving pages around.
    FossilOrigin-Name: 341c298508b00aea50c69462f9dfe9ebcede2e1710ad3b68f076d93f1ddb547c

---
+++ mtg_proxy_printer/document_controller/move_page.py
@@ -0,0 +1,71 @@
+#  Copyright © 2020-2025  Thomas Hess <thomas.hess@udo.edu>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+import functools
+import itertools
+import typing
+
+from PySide6.QtCore import QModelIndex
+
+from ._interface import DocumentAction, IllegalStateError, Self
+from mtg_proxy_printer.logger import get_logger
+
+if typing.TYPE_CHECKING:
+    from mtg_proxy_printer.model.document_page import Page
+    from mtg_proxy_printer.model.document import Document
+
+logger = get_logger(__name__)
+del get_logger
+__all__ = [
+    "ActionMovePage",
+]
+
+
+class ActionMovePage(DocumentAction):
+    """
+    Moves a page within the document from source_page to target_page
+    """
+
+    COMPARISON_ATTRIBUTES = ["source_page", "target_page"]
+
+    def __init__(self, source_page: int, target_page: int):
+        super().__init__()
+        self.source_page = source_page
+        self.target_page = target_page
+
+    def apply(self, document: "Document") -> Self:
+        super().apply(document)
+        self._validate_parameters(document)
+        return self
+
+    def undo(self, document: "Document") -> Self:
+        super().undo(document)
+        self._validate_parameters(document)
+        return self
+
+    def _validate_parameters(self, document: "Document"):
+        if not (
+                self.source_page >= 0 <= self.target_page and
+                self.source_page > document.rowCount() < self.target_page):
+            raise IllegalStateError()
+
+    @functools.cached_property
+    def as_str(self):
+        return self.translate(
+            "ActionMovePage",
+            "Move page {source_page} to position {target_page}",
+            "Both parameters are page numbers, like in 'Move page 3 to position 7'"
+        ).format(source_page=self.source_page+1, target_page=self.target_page+1)
---
+++ tests/document_controller/test_action_move_page.py
@@ -0,0 +1,126 @@
+#  Copyright © 2020-2025  Thomas Hess <thomas.hess@udo.edu>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from PySide6.QtCore import Qt
+
+import pytest
+from hamcrest import *
+
+from document_controller.helpers import append_new_card_in_page
+from mtg_proxy_printer.document_controller import IllegalStateError
+from mtg_proxy_printer.document_controller.move_page import ActionMovePage
+from mtg_proxy_printer.document_controller.page_actions import ActionNewPage
+from mtg_proxy_printer.model.document import Document
+from mtg_proxy_printer.model.document_page import PageColumns
+
+
+@pytest.fixture()
+def document_with_pages(document: Document) -> Document:
+    ActionNewPage(count=3).apply(document)
+    for number in range(3):
+        append_new_card_in_page(document.pages[number], f"{number}")
+    return document
+
+def _card_name_on_page(document: Document, page: int) -> str:
+    page_index = document.index(page, 0)
+    card_index = document.index(0, PageColumns.CardName, page_index)
+    return document.data(card_index, Qt.ItemDataRole.EditRole)
+
+
+@pytest.mark.parametrize("source_page, target_page, expected_order", [
+    (0, 0, "0123"),
+    (0, 1, "1023"),
+    (0, 2, "1203"),
+    (0, 3, "1230"),
+
+    (1, 0, "1023"),
+    (1, 1, "0123"),
+    (1, 2, "0213"),
+    (1, 3, "0231"),
+
+    (2, 0, "2013"),
+    (2, 1, "0213"),
+    (2, 2, "0123"),
+    (2, 3, "0132"),
+
+    (3, 0, "3012"),
+    (3, 1, "0312"),
+    (3, 2, "0132"),
+    (3, 3, "0123"),
+
+])
+def test_apply(document_with_pages: Document, source_page: int, target_page: int, expected_order: str):
+    pytest.skip()
+    action = ActionMovePage(source_page, target_page)
+    assert_that(action.apply(document_with_pages), is_(same_instance(action)))
+    assert_that(document_with_pages.rowCount(), is_(4))
+    for page, expected_on_page in enumerate(expected_order):
+        assert_that(_card_name_on_page(document_with_pages, page), is_(equal_to(expected_on_page)))
+
+@pytest.mark.parametrize("source_page, target_page", [
+    (-1, -1),
+    (-1, 0),
+    (0, -1),
+    (4, -1),
+    (4, 0),
+    (-1, 4),
+    (0, 4),
+])
+def test_apply_outside_range_raises_exception(document_with_pages: Document, source_page: int, target_page: int):
+    action = ActionMovePage(source_page, target_page)
+    assert_that(calling(action.apply).with_args(document_with_pages), raises(IllegalStateError))
+
+
+@pytest.mark.parametrize("target_page, source_page, expected_order", [  # source and target intentionally swapped
+    (0, 0, "0123"),
+    (0, 1, "1023"),
+    (0, 2, "1203"),
+    (0, 3, "1230"),
+
+    (1, 0, "1023"),
+    (1, 1, "0123"),
+    (1, 2, "0213"),
+    (1, 3, "0231"),
+
+    (2, 0, "2013"),
+    (2, 1, "0213"),
+    (2, 2, "0123"),
+    (2, 3, "0132"),
+
+    (3, 0, "3012"),
+    (3, 1, "0312"),
+    (3, 2, "0132"),
+    (3, 3, "0123"),
+])
+def test_undo(document_with_pages: Document, source_page: int, target_page: int, expected_order: list):
+    pytest.skip()
+    action = ActionMovePage(source_page, target_page)
+    assert_that(action.undo(document_with_pages), is_(same_instance(action)))
+    assert_that(document_with_pages.rowCount(), is_(4))
+    for page, expected_on_page in enumerate(expected_order):
+        assert_that(_card_name_on_page(document_with_pages, page), is_(equal_to(expected_on_page)))
+
+@pytest.mark.parametrize("source_page, target_page", [
+    (-1, -1),
+    (-1, 0),
+    (0, -1),
+    (4, -1),
+    (4, 0),
+    (-1, 4),
+    (0, 4),
+])
+def test_undo_outside_range_raises_exception(document_with_pages: Document, source_page: int, target_page: int):
+    action = ActionMovePage(source_page, target_page)
+    assert_that(calling(action.undo).with_args(document_with_pages), raises(IllegalStateError))
