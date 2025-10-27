e6e119b8b736bbbdcf6b05d81109514b853432e1
thomas <>
2025-10-27 15:22:51 +0000

    Fixed a crash occurring when moving a card from the first page and dropping it before the first page. This now correctly inserts a new page and move the card there.
    FossilOrigin-Name: a3d533e933d3336dbdf3877549e52d4642f017162a17e7c89b532b4df72e6150

--- mtg_proxy_printer/document_controller/move_cards.py
+++ mtg_proxy_printer/document_controller/move_cards.py
@@ -62,7 +62,7 @@ class ActionMoveCardsBetweenPages(DocumentAction):
         else:
             self.insert_page_action = None
         # When inserting a new page before the source page, add one to compensate
-        self.source_page = source + (target_page < source and self.insert_page_action is not None)
+        self.source_page = source + (target_page <= source and self.insert_page_action is not None)
         self.target_page = target_page
         self.target_row = target_row
         self.card_ranges_to_move = to_list_of_ranges(cards_to_move)
